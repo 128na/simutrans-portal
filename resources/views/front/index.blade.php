4b68d891379af0576acb181603dbc47229d9e6e0
128na <simutrans.128na@gmail.com>
2019-06-26 23:53:16 +0900

    記事が0件の時のメッセージを追加

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -5,29 +5,38 @@
 @section('content')
     <section class="mb-4">
         <h2 class="border-bottom">{{ __('message.latest') }}</h2>
-        @foreach ($articles['latest'] as $article)
+
+        @forelse ($articles['latest'] as $article)
             @include('front.articles.list-item', ['article' => $article])
-        @endforeach
-        <h6><a href="{{ route('addons.index') }}">{{ __('message.show-all-addons') }}</a></h6>
+            <h6><a href="{{ route('addons.index') }}">{{ __('message.show-all-addons') }}</a></h6>
+        @empty
+            <p>{{ __('message.no-article')}}</p>
+        @endforelse
     </section>
     <section class="mb-4">
         <h2 class="border-bottom">{{ __('message.random') }}</h2>
-        @foreach ($articles['random'] as $article)
+        @forelse ($articles['random'] as $article)
             @include('front.articles.list-item', ['article' => $article])
-        @endforeach
+        @empty
+            <p>{{ __('message.no-article')}}</p>
+        @endforelse
     </section>
     <section class="mb-4">
         <h2 class="border-bottom">{{ __('message.pages') }}</h2>
-        @foreach ($articles['pages'] as $article)
+        @forelse ($articles['pages'] as $article)
             @include('front.articles.list-item', ['article' => $article])
-        @endforeach
-        <h6><a href="{{ route('pages.index') }}">{{ __('message.show-all-pages') }}</a></h6>
+            <h6><a href="{{ route('pages.index') }}">{{ __('message.show-all-pages') }}</a></h6>
+        @empty
+            <p>{{ __('message.no-article')}}</p>
+        @endforelse
     </section>
     <section class="mb-4">
         <h2 class="border-bottom">{{ __('message.announces') }}</h2>
-        @foreach ($articles['announces'] as $article)
+        @forelse ($articles['announces'] as $article)
             @include('front.articles.list-item', ['article' => $article])
-        @endforeach
-        <h6><a href="{{ route('announces.index') }}">{{ __('message.show-all-announces') }}</a></h6>
+            <h6><a href="{{ route('announces.index') }}">{{ __('message.show-all-announces') }}</a></h6>
+        @empty
+            <p>{{ __('message.no-article')}}</p>
+        @endforelse
     </section>
 @endsection

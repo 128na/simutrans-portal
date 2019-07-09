3d19bf24fac348ee20d307869f8bd1c4bb3380c0
128na <simutrans.128na@gmail.com>
2019-07-09 16:28:21 +0900

    GAからコンバージョンインポートを実装、CarbonをImmutable版に変更

--- app/Console/Commands/ImportGA.php
+++ app/Console/Commands/ImportGA.php
@@ -2,7 +2,12 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Article;
+use App\Models\ConversionCount;
+use Carbon\CarbonPeriod;
+use Carbon\CarbonImmutable;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class ImportGA extends Command
 {
@@ -19,6 +24,8 @@ class ImportGA extends Command
      * @var string
      */
     protected $description = 'Import conversion count from Google Analytics';
+
+    private $article_set;
 
     /**
      * Create a new command instance.
@@ -39,95 +46,124 @@ class ImportGA extends Command
     {
         $analytics = $this->initializeAnalytics();
         $profile = $this->getFirstProfileId($analytics);
-        $results = $this->getResults($analytics, $profile);
-        $this->printResults($results);
+
+        // 取得できた最古のデータから今日まで
+        $period = new CarbonPeriod('2016-05-31', '1 days', today()->format('Y-m-d'));
+        /**
+         * immutable版を指定
+         * @see https://github.com/briannesbitt/Carbon/blob/master/src/Carbon/CarbonPeriod.php#L530
+         */
+        $period->setDateClass(CarbonImmutable::class);
+
+        // 記事をタイトルでインデックス
+        $this->article_set = collect([]);
+        Article::all()->map(function($article) {
+            $this->article_set->put($article->title, $article);
+        });
+
+        DB::beginTransaction();
+        foreach ($period as $date) {
+            $records = $this->getRecords($analytics, $profile, $date);
+
+            if(is_null($records)) {
+                $this->warn('nodata: '.$date->format('Y-m-d'));
+                continue;
+            }
+            foreach ($records as $record) {
+                $this->applyRecord($record, $date);
+            }
+        }
+        DB::commit();
+    }
+    private function applyRecord($record, $date)
+    {
+        $title = $record[0];
+        $count = $record[1];
+
+        $article = $this->article_set->get($title);
+
+        if(is_null($article)) {
+            return $this->warn('missing article: '.$title);
+        }
+        for ($i=0; $i < $count; $i++) {
+            ConversionCount::countUp($article, $date);
+        }
+        return $this->info("add : {$title}, {$count}");
     }
 
+    /**
+     * @see https://developers.google.com/analytics/devguides/reporting/core/v3/quickstart/service-php#helloanalyticsphp
+     */
     private function initializeAnalytics()
     {
-    // Creates and returns the Analytics Reporting service object.
+        // Creates and returns the Analytics Reporting service object.
 
-    // Use the developers console and download your service account
-    // credentials in JSON format. Place them in this directory or
-    // change the key file location if necessary.
-    $KEY_FILE_LOCATION = resource_path('apikey/ga.json');
+        // Use the developers console and download your service account
+        // credentials in JSON format. Place them in this directory or
+        // change the key file location if necessary.
+        $KEY_FILE_LOCATION = resource_path('apikey/ga.json');
 
-    // Create and configure a new client object.
-    $client = new \Google_Client();
-    $client->setApplicationName("Hello Analytics Reporting");
-    $client->setAuthConfig($KEY_FILE_LOCATION);
-    $client->setScopes(['https://www.googleapis.com/auth/analytics.readonly']);
-    $analytics = new \Google_Service_Analytics($client);
+        // Create and configure a new client object.
+        $client = new \Google_Client();
+        $client->setApplicationName("Hello Analytics Reporting");
+        $client->setAuthConfig($KEY_FILE_LOCATION);
+        $client->setScopes(['https://www.googleapis.com/auth/analytics.readonly']);
+        $analytics = new \Google_Service_Analytics($client);
 
-    return $analytics;
+        return $analytics;
     }
 
-    private function getFirstProfileId($analytics) {
-    // Get the user's first view (profile) ID.
+    private function getFirstProfileId($analytics)
+    {
+        // Get the user's first view (profile) ID.
 
-    // Get the list of accounts for the authorized user.
-    $accounts = $analytics->management_accounts->listManagementAccounts();
+        // Get the list of accounts for the authorized user.
+        $accounts = $analytics->management_accounts->listManagementAccounts();
 
-    if (count($accounts->getItems()) > 0) {
-        $items = $accounts->getItems();
-        $firstAccountId = $items[0]->getId();
+        if (count($accounts->getItems()) > 0) {
+            $items = $accounts->getItems();
+            $firstAccountId = $items[0]->getId();
 
-        // Get the list of properties for the authorized user.
-        $properties = $analytics->management_webproperties
-            ->listManagementWebproperties($firstAccountId);
+            // Get the list of properties for the authorized user.
+            $properties = $analytics->management_webproperties
+                ->listManagementWebproperties($firstAccountId);
 
-        if (count($properties->getItems()) > 0) {
-        $items = $properties->getItems();
-        $firstPropertyId = $items[0]->getId();
+            if (count($properties->getItems()) > 0) {
+            $items = $properties->getItems();
+            $firstPropertyId = $items[0]->getId();
 
-        // Get the list of views (profiles) for the authorized user.
-        $profiles = $analytics->management_profiles
-            ->listManagementProfiles($firstAccountId, $firstPropertyId);
+            // Get the list of views (profiles) for the authorized user.
+            $profiles = $analytics->management_profiles
+                ->listManagementProfiles($firstAccountId, $firstPropertyId);
 
-        if (count($profiles->getItems()) > 0) {
-            $items = $profiles->getItems();
+            if (count($profiles->getItems()) > 0) {
+                $items = $profiles->getItems();
 
-            // Return the first view (profile) ID.
-            return $items[0]->getId();
+                // Return the first view (profile) ID.
+                return $items[0]->getId();
 
+            } else {
+                throw new \Exception('No views (profiles) found for this user.');
+            }
+            } else {
+            throw new \Exception('No properties found for this user.');
+            }
         } else {
-            throw new \Exception('No views (profiles) found for this user.');
+            throw new \Exception('No accounts found for this user.');
         }
-        } else {
-        throw new \Exception('No properties found for this user.');
-        }
-    } else {
-        throw new \Exception('No accounts found for this user.');
-    }
     }
 
-    private function getResults($analytics, $profileId) {
-    // Calls the Core Reporting API and queries for the number of sessions
-    // for the last seven days.
-    return $analytics->data_ga->get(
-        'ga:' . $profileId,
-        '7daysAgo',
-        'today',
-        'ga:sessions');
+    private function getRecords($analytics, $profileId, $date)
+    {
+        // Calls the Core Reporting API and queries for the number of sessions
+        // for the last seven days.
+        $results = $analytics->data_ga->get(
+            "ga:{$profileId}",
+            $date->format('Y-m-d'),
+            $date->format('Y-m-d'),
+            'ga:totalEvents',
+            ['dimensions' => 'ga:EventLabel']);
+        return $results->getRows();
     }
 
-    private function printResults($results) {
-    // Parses the response from the Core Reporting API and prints
-    // the profile name and total sessions.
-    if (count($results->getRows()) > 0) {
-
-        // Get the profile name.
-        $profileName = $results->getProfileInfo()->getProfileName();
-
-        // Get the entry for the first entry in the first row.
-        $rows = $results->getRows();
-        $sessions = $rows[0][0];
-
-        // Print the results.
-        print "First view (profile) found: $profileName\n";
-        print "Total sessions: $sessions\n";
-    } else {
-        print "No results found.\n";
-    }
-    }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Carbon\CarbonImmutable;
+use Illuminate\Support\Facades\Date;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,5 +28,7 @@ class AppServiceProvider extends ServiceProvider
         // blade内の改行を有効にする
         // https://codeday.me/jp/qa/20190208/214590.html
         \Blade::setEchoFormat('nl2br(e(%s), false)');
+
+        Date::use(CarbonImmutable::class);
     }
 }

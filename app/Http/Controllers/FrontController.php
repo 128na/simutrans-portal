d9467f46f591786f944c07d584e955e4289c53fb
128na <simutrans.128na@gmail.com>
2019-07-10 20:57:20 +0900

    トップページで新着に表示されている記事をランキングで重複表示されないように修正

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -11,12 +11,18 @@ class FrontController extends Controller
 {
     public function index()
     {
+        $announces = Article::announce()->active()->withForList()->limit(3)->get();
+        $pages     = Article::withoutAnnounce()->active()->withForList()->limit(3)->get();
+        $latest    = Article::addon()->active()->withForList()->limit(3)->get();
+        $ranking   = Article::addon()->ranking()->active()->whereNotIn('articles.id', $latest->pluck('id'))->withForList()->limit(3)->get();
+
+
         $data = [
             'articles' => [
-                'announces' => Article::announce()->active()->withForList()->limit(3)->get(),
-                'pages'     => Article::withoutAnnounce()->active()->withForList()->limit(3)->get(),
-                'latest'    => Article::addon()->active()->withForList()->limit(3)->get(),
-                'ranking'   => Article::addon()->ranking()->active()->withForList()->limit(3)->get(),
+                'announces' => $announces,
+                'pages'     => $pages,
+                'latest'    => $latest,
+                'ranking'   => $ranking,
             ]
         ];
 

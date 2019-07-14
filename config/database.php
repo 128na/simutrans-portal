302a735a195276a9f86ae1d51ab3ed6f7af74701
128na <simutrans.128na@gmail.com>
2019-07-14 12:45:26 +0900

    mysqldumpのパスを指定するように修正

--- config/database.php
+++ config/database.php
@@ -61,6 +61,13 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+
+            /**
+             * @see https://docs.spatie.be/laravel-backup/v6/installation-and-setup/#dumping-the-database
+             */
+            'dump' => [
+                'dump_binary_path' => env('DUMP_BINARY_PATH', '/usr/local/bin/'),
+            ]
         ],
 
         'pgsql' => [

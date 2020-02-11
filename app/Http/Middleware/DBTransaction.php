dc85197647f53d04ee723ec596cb890dd32eb082
128na <simutrans.128na@gmail.com>
2020-02-11 12:35:11 +0900

    トランザクションをクロージャに変更

--- app/Http/Middleware/DBTransaction.php
+++ app/Http/Middleware/DBTransaction.php
@@ -3,8 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Response;
-use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 
 class DBTransaction
 {
@@ -18,18 +17,8 @@ class DBTransaction
      */
     public function handle($request, Closure $next)
     {
-        \DB::beginTransaction();
-        try {
-            $response = $next($request);
-        } catch (\Throwable $e) {
-            \DB::rollBack();
-            throw $e;
-        }
-        if (($response instanceof Response || $response instanceof JsonResponse) && $response->getStatusCode() > 399) {
-            \DB::rollBack();
-        } else {
-            \DB::commit();
-        }
-        return $response;
+        return DB::transaction(function () use ($next, $request) {
+            return $next($request);
+        }, 5);
     }
 }

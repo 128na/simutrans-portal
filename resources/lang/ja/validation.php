0fb6f0641e5d56581a0f3c8b449f5d7057baf6bf
128na <simutrans.128na@gmail.com>
2020-04-10 15:19:24 +0900

    attributesを共通化

--- resources/lang/ja/validation.php
+++ resources/lang/ja/validation.php
@@ -143,46 +143,5 @@ return [
     |
      */
 
-    'attributes' => [
-        //article
-        'article.post_type' => __('Post Type'),
-        'article.status' => __('Status'),
-        'article.title' => __('Title'),
-        'article.slug' => __('Slug'),
-        'article.contents' => __('Contents'),
-        'article.contents.thumbnail' => __('Thumbnail'),
-        'article.contents.author' => __('Author'),
-        'article.contents.file' => __('Addon File'),
-        'article.contents.link' => __('Link'),
-        'article.contents.description' => __('Description'),
-        'article.contents.license' => __('License other'),
-        'article.contents.thanks' => __('Acknowledgments and Referenced'),
-        'article.contents.agreement' => __('Agreement'),
-        'article.contents.sections.*.type' => __('Type'),
-        'article.contents.sections.*.text' => __('Text'),
-        'article.contents.sections.*.caption' => __('Caption'),
-        'article.contents.sections.*.url' => __('URL'),
-        'article.contents.sections.*.id' => __('Image'),
-        'article.categories' => __('Categories'),
-        'article.tags' => __('Tags'),
-        'article.contents.file' => __('File'),
-
-        //auth
-        'email' => __('Email'),
-        'name' => __('Name'),
-        'password' => __('Password'),
-
-        // profile
-        'user.email' => __('Email'),
-        'user.name' => __('Name'),
-        'user.profile.data.description' => __('Description'),
-        'user.profile.data.website' => __('Website URL'),
-        'user.profile.data.twitter' => __('Twitter ID'),
-
-        // analytics
-        'ids' => __('Article'),
-        'type' => __('Types'),
-        'start_date' => __('Start Date'),
-        'end_date' => __('End Date'),
-    ],
+    'attributes' => @include dirname(__DIR__) . '../_attributes.php' ?? [],
 ];

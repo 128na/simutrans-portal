fc71d929a474d5f664667c7f3a61662b02399153
128na <simutrans.128na@gmail.com>
2020-04-14 12:57:33 +0900

    SPレイアウトを調整

--- resources/views/parts/menu-footer.blade.php
+++ resources/views/parts/menu-footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="navbar-dark bg-primary py-1">
-    <div class="container d-flex text-white">
-        <small class="mr-2 text-white">{{ config('app.name') }} {{ config('app.version') }} © 2020 128Na</small>
-        <small class="mr-2"><a class="text-white" href="https://twitter.com/128Na" target="_blank" rel="noopener, nofollow">Twitter</a></small>
-        <small class="flex-fill"><a class="text-white" href="https://github.com/128na/simutrans-portal" target="_blank" rel="noopener, nofollow">GitHub</a></small>
-        <small><a class="text-white" href="{{ route('feeds.addon') }}" class="ml-2"><img src="{{ asset('storage/default/feed.png') }}" class="feed-icon">Atom</a></small>
+    <div class="container d-flex text-white flex-wrap">
+        <small class="mb-1 mr-2 text-white">{{ config('app.name') }} {{ config('app.version') }} © 2020 128Na</small>
+        <small class="mb-1 mr-2"><a class="text-white" href="https://twitter.com/128Na" target="_blank" rel="noopener, nofollow">Twitter</a></small>
+        <small class="mb-1 flex-fill"><a class="text-white" href="https://github.com/128na/simutrans-portal" target="_blank" rel="noopener, nofollow">GitHub</a></small>
+        <small class="mb-1"><a class="text-white" href="{{ route('feeds.addon') }}" class="ml-2"><img src="{{ asset('storage/default/feed.png') }}" class="feed-icon">Atom</a></small>
     </div>
 </footer>

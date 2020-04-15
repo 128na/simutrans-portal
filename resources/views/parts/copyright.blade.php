22261f184cbed6a9e46976dec0f3df7612e44aae
128na <simutrans.128na@gmail.com>
2020-04-15 20:31:06 +0900

    レイアウトを調整

--- resources/views/parts/copyright.blade.php
+++ resources/views/parts/copyright.blade.php
@@ -1,8 +1,10 @@
 <li class="nav-item">
     <small class="d-block mb-1 text-white">
-        {{ config('app.name') }}<br>
-        v{{ config('app.version') }} © 2020
-        <a class="text-white" href="https://twitter.com/128Na" target="_blank" rel="noopener, nofollow">128Na</a> /
+        {{ config('app.name') }}
+        v{{ config('app.version') }}
+    </small>
+    <small class="d-block mb-1 text-white">
+        © 2020 <a class="text-white" href="https://twitter.com/128Na" target="_blank" rel="noopener, nofollow">@128Na</a> /
         <a class="text-white" href="https://github.com/128na/simutrans-portal" target="_blank" rel="noopener, nofollow">GitHub</a>
     </small>
     <small class="d-block mb-1 text-white">

6a5df6fcd06b3a5510e9f7b4ee59d445a1655256
128na <simutrans.128na@gmail.com>
2020-04-16 10:30:46 +0900

    レイアウトを調整

--- resources/js/script.js
+++ resources/js/script.js
@@ -7,3 +7,12 @@ import LazyLoad from "vanilla-lazyload";
 new LazyLoad({
   elements_selector: ".lazy"
 });
+
+$('.articles .popover-thumbnail').popover({
+  html: true,
+  trigger: 'hover',
+  delay: { show: 500, hide: 100 },
+  content() {
+    return `<img class="img-fluid" src="${this.dataset.src}">`
+  }
+})
--- resources/views/components/articles-addon.blade.php
+++ resources/views/components/articles-addon.blade.php
@@ -3,30 +3,24 @@
     <div class="articles bg-white">
         @foreach ($articles as $article)
         <article class="pb-2">
-            <div class="h-100 d-flex flex-column justify-content-between">
-                <div>
-                    <a
-                        href="{{ route('articles.show', $article->slug)}}"
-                        class="thumbnail"
-                        style="background-image: url({{$article->thumbnail_url}})"
-                        title="{{$article->title}}"
-                    >
-                    </a>
-                    <a
-                        href="{{ route('articles.show', $article->slug)}}"
-                        title="{{$article->title}}"
-                        itemprop="url">
-                        <h3 class="m-1 title" itemprop="name">{{ $article->title }}</h3>
-                    </a>
-                </div>
-                <div class="px-1 text-secondary">
-                    <div>
-                        By <a class="text-secondary" href="{{ route('user', [$article->user]) }}">{{ $article->user->name}}</a>
-                    </div>
-                    <div>
-                        @include('parts.category-list', ['categories' => $article->category_addons->splice(0, 3)])
-                    </div>
-                </div>
+            <a
+                href="{{ route('articles.show', $article->slug)}}"
+                class="thumbnail popover-thumbnail"
+                style="background-image: url({{$article->thumbnail_url}})"
+                data-src="{{$article->thumbnail_url}}"
+            >
+            </a>
+            <a
+                href="{{ route('articles.show', $article->slug)}}"
+                title="{{$article->title}}"
+                itemprop="url">
+                <h3 class="m-1 title" itemprop="name">{{ $article->title }}</h3>
+            </a>
+            <div class="px-1">
+                <a class="text-secondary" href="{{ route('user', [$article->user]) }}">{{ $article->user->name}}</a>
+            </div>
+            <div class="px-1">
+                @include('parts.category-list', ['categories' => $article->category_addons->splice(0, 3)])
             </div>
         </article>
     @endforeach

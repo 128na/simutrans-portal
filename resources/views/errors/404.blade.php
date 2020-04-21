cfcf2439e93a9d0cfcc9ed4598f1e46b1dbc0017
128na <simutrans.128na@gmail.com>
2020-04-21 10:18:04 +0900

    エラーページを調整

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -3,6 +3,7 @@
 @section('title', __('Not found'))
 
 @section('content')
+    <h2>@lang('Not found')</h2>
     <p>
         <a href="{{ route('index') }}">@lang('Top')</a>
     </p>
--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,3 +1,7 @@
 @extends('layouts.front')
 
 @section('title', __('Under maintenance'))
+
+@section('content')
+    <h2>@lang('Under maintenance')</h2>
+@endsection

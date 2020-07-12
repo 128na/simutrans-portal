7e4507c6825b0e00e82273d6015170681dddcd56
128na <simutrans.128na@gmail.com>
2020-07-12 18:49:14 +0900

    ハッシュタグからドットを除去するように変更、通知データ生成処理を共通化

---
+++ app/Notifications/ArticleNotification.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Channels\TwitterChannel;
+use App\Models\Article;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+abstract class ArticleNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [TwitterChannel::class];
+    }
+
+    /**
+     * @param  Article  $article
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toTwitter($article)
+    {
+        $url = route('articles.show', $article->slug);
+        $now = now()->format('Y/m/d H:i');
+        $name = $article->user->profile->has_twitter
+        ? '@' . $article->user->profile->data->twitter
+        : $article->user->name;
+        $tags = collect('simutrans')
+            ->merge($article->categoryPaks->pluck('name'))
+            ->map(fn ($name) => "#$name")
+            ->map(fn ($name) => str_replace('.', '', $name))
+            ->implode(', ');
+
+        $message = __(
+            $this->getMessage(),
+            ['title' => $article->title, 'url' => $url, 'name' => $name, 'at' => $now, 'tags' => $tags]
+        );
+        return $message;
+    }
+
+    abstract protected function getMessage():string;
+}

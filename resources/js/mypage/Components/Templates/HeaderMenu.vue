2f94d32046e1c602aa794178bcce1078c971ca0f
128na <simutrans.128na@gmail.com>
2020-11-03 22:49:04 +0900

    不要なアイコンを削除

--- resources/js/mypage/Components/Templates/HeaderMenu.vue
+++ resources/js/mypage/Components/Templates/HeaderMenu.vue
@@ -5,7 +5,7 @@
     variant="primary"
     toggleable="lg"
   >
-    <b-navbar-brand class="p-0 mb-lg -4 mb-0" :href="top_url">
+    <b-navbar-brand class="p-0 mb-lg mb-2 mb-0" :href="top_url">
       Simutrans Addon Portal
     </b-navbar-brand>
 
@@ -16,12 +16,13 @@
         <b-nav-item class="active" v-if="isAdmin" :href="admin_url">
           管理者画面
         </b-nav-item>
+        <b-nav-item class="active" :to="route_mypage_index">
+          マイページトップ
+        </b-nav-item>
         <b-nav-item class="active" v-if="isVerified" :to="route_edit_profile">
-          <b-icon icon="person" class="mr-1" />
           プロフィール編集
         </b-nav-item>
         <b-nav-item class="active" v-if="isVerified" :to="route_analytics">
-          <b-icon icon="graph-up" class="mr-1" />
           アクセス解析
         </b-nav-item>
         <b-nav-item
@@ -29,7 +30,6 @@
           v-if="isVerified"
           :to="route_create_addon_post"
         >
-          <icon-edit-article class="mr-1" />
           アドオン投稿を作成
         </b-nav-item>
         <b-nav-item
@@ -37,11 +37,9 @@
           v-if="isVerified"
           :to="route_create_addon_introduction"
         >
-          <icon-edit-article class="mr-1" />
           アドオン紹介を作成
         </b-nav-item>
         <b-nav-item class="active" v-if="isVerified" :to="route_create_page">
-          <icon-edit-article class="mr-1" />
           一般記事を作成
         </b-nav-item>
         <b-nav-item
@@ -49,18 +47,14 @@
           v-if="isVerified"
           :to="route_create_markdown"
         >
-          <icon-edit-article class="mr-1" />
           一般記事(markdown)を作成
         </b-nav-item>
         <div class="dropdown-divider border-light" />
-        <b-nav-item class="active" @click="logout">
-          <b-icon icon="box-arrow-right" class="mr-1" />
-          ログアウト
-        </b-nav-item>
+        <b-nav-item class="active" @click="logout">ログアウト</b-nav-item>
       </b-navbar-nav>
       <b-navbar-nav v-else>
-        <b-nav-item class="active" :to="route_login"> ログイン </b-nav-item>
-        <b-nav-item class="active" :to="route_register"> 新規登録 </b-nav-item>
+        <b-nav-item class="active" :to="route_login">ログイン</b-nav-item>
+        <b-nav-item class="active" :to="route_register">新規登録</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>

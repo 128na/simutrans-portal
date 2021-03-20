329a04eef0b0a06f9da6b806e64b36bab589b372
128na <simutrans.128na@gmail.com>
2021-03-20 16:48:29 +0900

    ApiArticleTestを更新

--- tests/Feature/Api/v2/Article/CategoryTest.php
+++ tests/Feature/Api/v2/Article/CategoryTest.php
@@ -2,36 +2,29 @@
 
 namespace Tests\Feature\Api\v2\Article;
 
-use App\Models\Article;
 use App\Models\Category;
-use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class CategoryTest extends TestCase
 {
-    use RefreshDatabase;
-
-    protected function setUp(): void
+    /**
+     * @dataProvider dataStatus
+     */
+    public function testShow(string $status, bool $should_see)
     {
-        parent::setUp();
-        $this->seed('ProdSeeder');
-        // シード内の記事作成と作成時間をずらす
-        sleep(1);
-    }
-
-    public function testShow()
-    {
-        $user = User::factory()->create();
-        $article = Article::factory()->create(['user_id' => $user->id, 'status' => 'publish']);
+        $this->article->fill(['status' => $status])->save();
         $category = Category::first();
-        $article->categories()->attach($category->id);
+        $this->article->categories()->attach($category->id);
 
         $url = route('api.v2.articles.category', $category);
 
         $res = $this->getJson($url);
         $res->assertStatus(200);
-        $res->assertJsonFragment(['title' => $article->title]);
+        if ($should_see) {
+            $res->assertJsonFragment(['title' => $this->article->title]);
+        } else {
+            $res->assertJsonMissing(['title' => $this->article->title]);
+        }
     }
 
     public function testInvalid()
@@ -40,47 +33,5 @@ class CategoryTest extends TestCase
 
         $res = $this->getJson($url);
         $res->assertStatus(404);
-    }
-
-    public function testVisibilityDraft()
-    {
-        $user = User::factory()->create();
-        $article = Article::factory()->create(['user_id' => $user->id, 'status' => 'draft']);
-        $category = Category::first();
-        $article->categories()->attach($category->id);
-
-        $url = route('api.v2.articles.category', $category);
-        $res = $this->getJson($url);
-        $res->assertStatus(200);
-
-        $res->assertJsonMissing(['title' => $article->title]);
-    }
-
-    public function testVisibilityPrivate()
-    {
-        $user = User::factory()->create();
-        $article = Article::factory()->create(['user_id' => $user->id, 'status' => 'private']);
-        $category = Category::first();
-        $article->categories()->attach($category->id);
-
-        $url = route('api.v2.articles.category', $category);
-        $res = $this->getJson($url);
-        $res->assertStatus(200);
-
-        $res->assertJsonMissing(['title' => $article->title]);
-    }
-
-    public function testVisibilityTrash()
-    {
-        $user = User::factory()->create();
-        $article = Article::factory()->create(['user_id' => $user->id, 'status' => 'trash']);
-        $category = Category::first();
-        $article->categories()->attach($category->id);
-
-        $url = route('api.v2.articles.category', $category);
-        $res = $this->getJson($url);
-        $res->assertStatus(200);
-
-        $res->assertJsonMissing(['title' => $article->title]);
     }
 }

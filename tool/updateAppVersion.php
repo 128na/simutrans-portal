4dc68f810eaa803acc712a3dfca292bee70e4994
128na <simutrans.128na@gmail.com>
2021-03-21 16:24:35 +0900

    バージョン取得をコマンドに変更

--- tool/updateAppVersion.php
+++
@@ -1,7 +0,0 @@
-<?php
-$version = trim(fgets(STDIN));
-$path = __DIR__.'/../.env';
-
-$env = file_get_contents($path);
-$new_env = preg_replace("/(APP_VERSION).*/",'$1='.$version, $env);
-file_put_contents($path, $new_env);

d78a5d991febf0ac2a4e25615e1f8c2c3522e1a8
128na <simutrans.128na@gmail.com>
2021-03-21 12:40:11 +0900

    addon　post test を更新

--- tests/Feature/Api/v2/Mypage/Article/UpdateAddonPostTest.php
+++ tests/Feature/Api/v2/Mypage/Article/UpdateAddonPostTest.php
@@ -7,24 +7,73 @@ use App\Models\Attachment;
 use App\Models\Category;
 use App\Models\Tag;
 use App\Models\User;
+use Closure;
 use Illuminate\Http\UploadedFile;
 use Tests\TestCase;
 
 class UpdateAddonPostTest extends TestCase
 {
-    public function testValidation()
+    private User $user2;
+    private Article $article2;
+    private Attachment $file_attachment;
+    private Attachment $user2_attachment;
+    private Attachment $user2_file;
+
+    protected function setUp(): void
     {
-        $user = User::factory()->create();
-        $article = $this->createAddonPost($user);
-        $url = route('api.v2.articles.update', $article);
+        parent::setUp();
+        $this->article = $this->createAddonPost();
+        $this->user2 = User::factory()->create();
+        $this->article2 = $this->createAddonPost($this->user2);
+        $this->file_attachment = Attachment::createFromFile(UploadedFile::fake()->create('other.zip', 1), $this->user->id);
+        $this->user2_attachment = Attachment::createFromFile(UploadedFile::fake()->image('other.png', 1), $this->user2->id);
+        $this->user2_file = Attachment::createFromFile(UploadedFile::fake()->image('other.zip', 1), $this->user2->id);
+    }
 
-        $res = $this->postJson($url);
-        $res->assertUnauthorized();
+    public function dataValidation()
+    {
+        yield 'ステータスが空' => [fn () => ['status' => ''], 'article.status'];
+        yield '不正なステータス' => [fn () => ['status' => 'test_example'], 'article.status'];
 
-        $this->actingAs($user);
+        yield 'タイトルが空' => [fn () => ['title' => ''], 'article.title'];
+        yield 'タイトルが256文字以上' => [fn () => ['title' => str_repeat('a', 256)], 'article.title'];
+        yield 'タイトルが重複' => [fn () => ['title' => $this->article2->title], 'article.title'];
 
-        $thumbnail = Attachment::createFromFile(UploadedFile::fake()->image('thumbnail.jpg', 1), $user->id);
-        $addon = Attachment::createFromFile(UploadedFile::fake()->create('addon.zip', 1), $user->id);
+        yield 'スラッグが空' => [fn () => ['slug' => ''], 'article.slug'];
+        yield 'スラッグが256文字以上' => [fn () => ['slug' => str_repeat('a', 256)], 'article.slug'];
+        yield 'スラッグが重複' => [fn () => ['slug' => $this->article2->slug], 'article.slug'];
+
+        yield '存在しないサムネイルID' => [fn () => ['contents' => ['thumbnail' => 99999]], 'article.contents.thumbnail'];
+        yield '画像以外' => [fn () => ['contents' => ['thumbnail' => $this->file_attachment->id]], 'article.contents.thumbnail'];
+        yield '他人の投稿したサムネイルID' => [fn () => ['contents' => ['thumbnail' => $this->user2_attachment->id]], 'article.contents.thumbnail'];
+        yield 'アドオン作者が256文字以上' => [fn () => ['contents' => ['author' => str_repeat('a', 256)]], 'article.contents.author'];
+
+        yield 'ファイルIDが空' => [fn () => ['contents' => ['file' => '']], 'article.contents.file'];
+        yield '存在しないファイルID' => [fn () => ['contents' => ['file' => 99999]], 'article.contents.file'];
+        yield '他人の投稿したファイルID' => [fn () => ['contents' => ['file' => $this->user2_file->id]], 'article.contents.file'];
+
+        yield '説明が空' => [fn () => ['contents' => ['description' => '']], 'article.contents.description'];
+        yield '説明が2049文字以上' => [fn () => ['contents' => ['description' => str_repeat('a', 2049)]], 'article.contents.description'];
+        yield '謝辞が2049文字以上' => [fn () => ['contents' => ['thanks' => str_repeat('a', 2049)]], 'article.contents.thanks'];
+        yield 'ライセンス（その他）が2049文字以上' => [fn () => ['contents' => ['license' => str_repeat('a', 2049)]], 'article.contents.license'];
+        yield 'タグ名が空' => [fn () => ['tags' => null], 'article.tags'];
+        yield '存在しないタグ名' => [fn () => ['tags' => ['missing_tag']], 'article.tags.0'];
+        yield 'タグ名が256文字以上' => [fn () => ['tags' => [str_repeat('a', 256)]], 'article.tags.0'];
+        yield 'カテゴリが空' => [fn () => ['categories' => null], 'article.categories'];
+        yield '存在しないカテゴリ' => [fn () => ['categories' => [99999]], 'article.categories.0'];
+        yield 'OK' => [fn () => [], null];
+    }
+
+    /**
+     * @dataProvider dataValidation
+     */
+    public function testValidation(Closure $fn, ?string $error_field)
+    {
+        $url = route('api.v2.articles.update', $this->article);
+        $this->actingAs($this->user);
+
+        $thumbnail = Attachment::createFromFile(UploadedFile::fake()->image('thumbnail.jpg', 1), $this->user->id);
+        $addon = Attachment::createFromFile(UploadedFile::fake()->create('addon.zip', 1), $this->user->id);
 
         $date = now()->format('YmdHis');
         $data = [
@@ -51,124 +100,25 @@ class UpdateAddonPostTest extends TestCase
             ],
         ];
 
-        // ステータスが空
-        $res = $this->postJson($url, ['article' => array_merge($data, ['status' => ''])]);
-        $res->assertJsonValidationErrors(['article.status']);
-        // 不正なステータス
-        $res = $this->postJson($url, ['article' => array_merge($data, ['status' => 'test_example'])]);
-        $res->assertJsonValidationErrors(['article.status']);
+        $data = array_merge($data, Closure::bind($fn, $this)());
 
-        // タイトルが空
-        $res = $this->postJson($url, ['article' => array_merge($data, ['title' => ''])]);
-        $res->assertJsonValidationErrors(['article.title']);
-        // タイトルが256文字以上
-        $res = $this->postJson($url, ['article' => array_merge($data, ['title' => str_repeat('a', 256)])]);
-        $res->assertJsonValidationErrors(['article.title']);
-        // タイトルが重複
-        $other_article = Article::factory()->create(['user_id' => User::factory()->create()->id]);
-        $res = $this->postJson($url, ['article' => array_merge($data, ['title' => $other_article->title])]);
-        $res->assertJsonValidationErrors(['article.title']);
-
-        // スラッグが空
-        $res = $this->postJson($url, ['article' => array_merge($data, ['slug' => ''])]);
-        $res->assertJsonValidationErrors(['article.slug']);
-        // スラッグが256文字以上
-        $res = $this->postJson($url, ['article' => array_merge($data, ['slug' => str_repeat('a', 256)])]);
-        $res->assertJsonValidationErrors(['article.slug']);
-        // スラッグが重複
-        $other_article = Article::factory()->create(['user_id' => User::factory()->create()->id]);
-        $res = $this->postJson($url, ['article' => array_merge($data, ['slug' => $other_article->slug])]);
-        $res->assertJsonValidationErrors(['article.slug']);
-
-        // 存在しないサムネイルID
-        $res = $this->postJson($url, ['article' => array_merge($data, ['contents' => ['thumbnail' => 99999]])]);
-        $res->assertJsonValidationErrors(['article.contents.thumbnail']);
-        // 画像以外
-        $file_attachment = Attachment::createFromFile(UploadedFile::fake()->create('other.zip', 1), $user->id);
-        $res = $this->postJson($url, ['article' => array_merge($data, ['contents' => ['thumbnail' => $file_attachment->id]])]);
-        $res->assertJsonValidationErrors(['article.contents.thumbnail']);
-        // 他人の投稿したサムネイルID
-        $others_attachment = Attachment::createFromFile(UploadedFile::fake()->image('other.png', 1), User::factory()->create()->id);
-        $res = $this->postJson($url, ['article' => array_merge($data, ['contents' => ['thumbnail' => $others_attachment->id]])]);
-        $res->assertJsonValidationErrors(['article.contents.thumbnail']);
-
-        // アドオン作者が256文字以上
-        $res = $this->postJson($url, ['article' => array_merge($data, ['contents' => ['author' => str_repeat('a', 256)]])]);
-        $res->assertJsonValidationErrors(['article.contents.author']);
-
-        // ファイルIDが空
-        $res = $this->postJson($url, ['article' => array_merge($data, ['contents' => ['file' => '']])]);
-        $res->assertJsonValidationErrors(['article.contents.file']);
-        // 存在しないファイルID
-        $res = $this->postJson($url, ['article' => array_merge($data, ['contents' => ['file' => 99999]])]);
-        $res->assertJsonValidationErrors(['article.contents.file']);
-        // 他人の投稿したファイルID
-        $others_attachment = Attachment::createFromFile(UploadedFile::fake()->create('other.zip', 1), User::factory()->create()->id);
-        $res = $this->postJson($url, ['article' => array_merge($data, ['contents' => ['file' => $others_attachment->id]])]);
-        $res->assertJsonValidationErrors(['article.contents.file']);
-
-        // 説明が空
-        $res = $this->postJson($url, ['article' => array_merge($data, ['contents' => ['description' => '']])]);
-        $res->assertJsonValidationErrors(['article.contents.description']);
-        // 説明が2049文字以上
-        $res = $this->postJson($url, ['article' => array_merge($data, ['contents' => ['description' => str_repeat('a', 2049)]])]);
-        $res->assertJsonValidationErrors(['article.contents.description']);
-
-        // 謝辞が2049文字以上
-        $res = $this->postJson($url, ['article' => array_merge($data, ['contents' => ['thanks' => str_repeat('a', 2049)]])]);
-        $res->assertJsonValidationErrors(['article.contents.thanks']);
-
-        // ライセンス（その他）が2049文字以上
-        $res = $this->postJson($url, ['article' => array_merge($data, ['contents' => ['license' => str_repeat('a', 2049)]])]);
-        $res->assertJsonValidationErrors(['article.contents.license']);
-
-        // タグ名が空
-        $res = $this->postJson($url, ['article' => array_merge($data, ['tags' => null])]);
-        $res->assertJsonValidationErrors(['article.tags']);
-        // 存在しないタグ名
-        $res = $this->postJson($url, ['article' => array_merge($data, ['tags' => ['missing_tag']])]);
-        $res->assertJsonValidationErrors(['article.tags.0']);
-        // タグ名が256文字以上
-        $res = $this->postJson($url, ['article' => array_merge($data, ['tags' => [str_repeat('a', 256)]])]);
-        $res->assertJsonValidationErrors(['article.tags.0']);
-
-        // カテゴリが空
-        $res = $this->postJson($url, ['article' => array_merge($data, ['categories' => null])]);
-        $res->assertJsonValidationErrors(['article.categories']);
-
-        // 存在しないカテゴリ
-        $res = $this->postJson($url, ['article' => array_merge($data, ['categories' => [99999]])]);
-        $res->assertJsonValidationErrors(['article.categories.0']);
-
-        // 適切なデータ
         $res = $this->postJson($url, ['article' => $data]);
-        $res->assertStatus(200);
-        $get_response = json_decode($this->getJson(route('api.v2.articles.index'))->content(), true);
-        $res->assertJson($get_response);
-    }
-
-    public function testPermission()
-    {
-        $user = User::factory()->create();
-        $this->actingAs($user);
-
-        $other_user = User::factory()->create();
-        $other_article = $this->createAddonPost($other_user);
-        $url = route('api.v2.articles.update', $other_article);
-
-        $res = $this->postJson($url);
-        $res->assertForbidden();
+        if (is_null($error_field)) {
+            $res->assertStatus(200);
+            $get_response = json_decode($this->getJson(route('api.v2.articles.index'))->content(), true);
+            $res->assertJson($get_response);
+        } else {
+            $res->assertJsonValidationErrors($error_field);
+        }
     }
 
     public function testPreview()
     {
-        $user = User::factory()->create();
-        $article = $this->createAddonPost($user);
-        $url = route('api.v2.articles.update', $article);
-        $this->actingAs($user);
+        $url = route('api.v2.articles.update', $this->article);
+        $this->actingAs($this->user);
 
-        $thumbnail = Attachment::createFromFile(UploadedFile::fake()->image('thumbnail.jpg', 1), $user->id);
-        $addon = Attachment::createFromFile(UploadedFile::fake()->create('addon.zip', 1), $user->id);
+        $thumbnail = Attachment::createFromFile(UploadedFile::fake()->image('thumbnail.jpg', 1), $this->user->id);
+        $addon = Attachment::createFromFile(UploadedFile::fake()->create('addon.zip', 1), $this->user->id);
 
         $date = now()->format('YmdHis');
         $data = [

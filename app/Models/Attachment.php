4300075ce488f15f443e61b14057c1e831d255b3
128na <simutrans.128na@gmail.com>
2021-04-06 18:00:05 +0900

    ArticleRepositoryのテストを追加

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
 
 class Attachment extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'user_id',
         'attachmentable_id',

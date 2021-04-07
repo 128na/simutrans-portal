daf7dfee1666ab8e981a484fd864f4503d840929
128na <simutrans.128na@gmail.com>
2021-04-07 14:06:32 +0900

    リレーション定義の型指定を追加

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
@@ -13,7 +14,7 @@ class Tag extends Model
         'name',
     ];
 
-    public function articles()
+    public function articles(): BelongsToMany
     {
         return $this->belongsToMany(Article::class);
     }

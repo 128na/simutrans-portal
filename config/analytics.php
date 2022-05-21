58ecd4457c3db20a57dee1f32468a5eb14d1ce33
128na <simutrans.128na@gmail.com>
2022-05-21 18:04:14 +0900

    CI用にダミーファイル追加

--- config/analytics.php
+++ config/analytics.php
@@ -2,6 +2,6 @@
 
 return [
     'view_id' => '119568071',
-    'key_location' => './gcp_service_account.json',
+    'key_location' => env('GCP_KEY_LOCATION', './gcp_service_account.json'),
     'scopes' => ['https://www.googleapis.com/auth/analytics.readonly'],
 ];

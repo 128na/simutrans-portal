6f57ab1293c4321945a678b01e5c7faf7db4a303
128na <simutrans.128na@gmail.com>
2022-05-22 17:29:19 +0900

    プライバシーポリシーとつい買い方ページへの固定リンクを追加

--- resources/views/parts/copyright.blade.php
+++ resources/views/parts/copyright.blade.php
@@ -1,10 +1,16 @@
 <li class="nav-item">
     <small class="d-block mb-1 text-white">
+        <a class="text-white" href="{{ url('/articles/about') }}">サイトの使い方</a> /
+        <a class="text-white" href="{{ url('/articles/privacy') }}">プライバシーポリシー</a>
+    </small>
+
+    <small class="d-block mb-1 text-white">
         {{ config('app.name') }}
         v{{ config('app.version') }}
     </small>
     <small class="d-block mb-1 text-white">
-        © 2020 <a class="text-white" href="https://twitter.com/128Na" target="_blank" rel="noopener nofollow">@128Na</a>
+        © 2020 <a class="text-white" href="https://twitter.com/128Na" target="_blank"
+            rel="noopener nofollow">@128Na</a>
         /
         <a class="text-white" href="https://github.com/128na/simutrans-portal" target="_blank"
             rel="noopener nofollow">GitHub</a>

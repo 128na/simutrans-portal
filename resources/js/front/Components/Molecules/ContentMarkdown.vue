e3fb340a3733f3aa39f1fc4a240060ce6e531cbe
128na <simutrans.128na@gmail.com>
2022-08-21 03:04:57 +0900

    サニタイズ実装

--- resources/js/front/Components/Molecules/ContentMarkdown.vue
+++ resources/js/front/Components/Molecules/ContentMarkdown.vue
@@ -1,9 +1,20 @@
 <template>
-  <div v-html="parsed" />
+  <div>
+    <div v-html="sanitized" />
+  </div>
 </template>
 <script>
 import MarkdownIt from 'markdown-it';
-const md = new MarkdownIt();
+import sanitizeHtml from 'sanitize-html';
+
+// https://github.com/markdown-it/markdown-it
+const md = new MarkdownIt({
+  html: true,
+  breaks: true,
+  linkify: true,
+  typographer: true
+});
+
 export default {
   props: {
     article: {
@@ -14,6 +25,27 @@ export default {
   computed: {
     parsed() {
       return md.render(this.article.contents.markdown);
+    },
+    sanitized() {
+      // https://www.npmjs.com/package/sanitize-html
+      // HTMLPurifierと同等設定
+      return sanitizeHtml(this.parsed, {
+        allowedTags: [
+          'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+          'hr',
+          'pre', 'code',
+          'blockquote',
+          'table', 'tr', 'td', 'th', 'thead', 'tbody',
+          'strong', 'em', 'b', 'i', 'u', 's', 'span',
+          'a', 'p', 'br',
+          'ul', 'ol', 'li',
+          'img'
+        ],
+        allowedAttributes: {
+          a: ['href', 'name', 'target'],
+          img: ['src', 'srcset', 'alt', 'title', 'width', 'height', 'loading']
+        }
+      });
     }
   }
 };

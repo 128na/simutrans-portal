632928039586d7f5d38e19ed6bd5638aeb7a1edf
128na <simutrans.128na@gmail.com>
2022-09-18 22:46:28 +0900

    add icon

--- resources/js/front/Components/Atoms/MessageError.vue
+++ resources/js/front/Components/Atoms/MessageError.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <b-icon icon="exclamation-triangle" font-scale="1.1" class="mr-1" />
     読み込みに失敗しました。
   </div>
 </template>
--- resources/js/front/plugins/index.js
+++ resources/js/front/plugins/index.js
@@ -2,93 +2,96 @@ import Vue from 'vue';
 
 import {
   AlertPlugin,
+  BIcon,
+  BIconArrowDown,
+  BIconArrowLeft,
+  BIconBookmarkStarFill,
+  BIconBoxArrowUpRight,
+  BIconClipboardData,
+  BIconExclamationTriangle,
+  BIconEyeFill,
+  BIconEyeSlashFill,
+  BIconFileEarmarkTextFill,
+  BIconFileEarmarkZipFill,
+  BIconGraphUp,
+  BIconHouseFill,
+  BIconKeyFill,
+  BIconLockFill,
+  BIconPencil,
+  BIconPersonFill,
+  BIconPersonPlusFill,
+  BIconTrash,
+  BIconUnlockFill,
   BadgePlugin,
-  ButtonPlugin,
   ButtonGroupPlugin,
+  ButtonPlugin,
   CardPlugin,
   CollapsePlugin,
   DropdownPlugin,
-  FormPlugin,
   FormCheckboxPlugin,
   FormDatepickerPlugin,
-  FormTimepickerPlugin,
   FormGroupPlugin,
   FormInputPlugin,
+  FormPlugin,
   FormRadioPlugin,
   FormSelectPlugin,
   FormTagsPlugin,
   FormTextareaPlugin,
+  FormTimepickerPlugin,
   ImagePlugin,
   InputGroupPlugin,
   ModalPlugin,
   NavPlugin,
   NavbarPlugin,
   OverlayPlugin,
-  TablePlugin,
+  PaginationNavPlugin,
   SpinnerPlugin,
-  BIcon,
-  BIconEyeFill,
-  BIconEyeSlashFill,
-  BIconArrowLeft,
-  BIconHouseFill,
-  BIconPersonFill,
-  BIconGraphUp,
-  BIconFileEarmarkZipFill,
-  BIconFileEarmarkTextFill,
-  BIconClipboardData,
-  BIconBoxArrowUpRight,
-  BIconPencil,
-  BIconLockFill,
-  BIconUnlockFill,
-  BIconArrowDown,
-  BIconBookmarkStarFill,
-  BIconTrash,
-  BIconKeyFill,
-  BIconPersonPlusFill,
-  PaginationNavPlugin
+  TablePlugin
 } from 'bootstrap-vue';
-Vue.use(PaginationNavPlugin);
+
+Vue.component('BIcon', BIcon);
+Vue.component('BIconArrowDown', BIconArrowDown);
+Vue.component('BIconArrowLeft', BIconArrowLeft);
+Vue.component('BIconBookmarkStarFill', BIconBookmarkStarFill);
+Vue.component('BIconBoxArrowUpRight', BIconBoxArrowUpRight);
+Vue.component('BIconClipboardData', BIconClipboardData);
+Vue.component('BIconExclamationTriangle', BIconExclamationTriangle);
+Vue.component('BIconEyeFill', BIconEyeFill);
+Vue.component('BIconEyeSlashFill', BIconEyeSlashFill);
+Vue.component('BIconFileEarmarkTextFill', BIconFileEarmarkTextFill);
+Vue.component('BIconFileEarmarkZipFill', BIconFileEarmarkZipFill);
+Vue.component('BIconGraphUp', BIconGraphUp);
+Vue.component('BIconHouseFill', BIconHouseFill);
+Vue.component('BIconKeyFill', BIconKeyFill);
+Vue.component('BIconLockFill', BIconLockFill);
+Vue.component('BIconPencil', BIconPencil);
+Vue.component('BIconPersonFill', BIconPersonFill);
+Vue.component('BIconPersonPlusFill', BIconPersonPlusFill);
+Vue.component('BIconTrash', BIconTrash);
+Vue.component('BIconUnlockFill', BIconUnlockFill);
 Vue.use(AlertPlugin);
 Vue.use(BadgePlugin);
-Vue.use(ButtonPlugin);
 Vue.use(ButtonGroupPlugin);
+Vue.use(ButtonPlugin);
 Vue.use(CardPlugin);
 Vue.use(CollapsePlugin);
 Vue.use(DropdownPlugin);
-Vue.use(FormPlugin);
 Vue.use(FormCheckboxPlugin);
 Vue.use(FormDatepickerPlugin);
-Vue.use(FormTimepickerPlugin);
 Vue.use(FormGroupPlugin);
 Vue.use(FormInputPlugin);
+Vue.use(FormPlugin);
 Vue.use(FormRadioPlugin);
 Vue.use(FormSelectPlugin);
 Vue.use(FormTagsPlugin);
 Vue.use(FormTextareaPlugin);
+Vue.use(FormTimepickerPlugin);
 Vue.use(ImagePlugin);
 Vue.use(InputGroupPlugin);
 Vue.use(ModalPlugin);
 Vue.use(NavPlugin);
 Vue.use(NavbarPlugin);
 Vue.use(OverlayPlugin);
-Vue.use(TablePlugin);
+Vue.use(PaginationNavPlugin);
 Vue.use(SpinnerPlugin);
-Vue.component('BIcon', BIcon);
-Vue.component('BIconEyeFill', BIconEyeFill);
-Vue.component('BIconEyeSlashFill', BIconEyeSlashFill);
-Vue.component('BIconArrowLeft', BIconArrowLeft);
-Vue.component('BIconHouseFill', BIconHouseFill);
-Vue.component('BIconPersonFill', BIconPersonFill);
-Vue.component('BIconGraphUp', BIconGraphUp);
-Vue.component('BIconFileEarmarkZipFill', BIconFileEarmarkZipFill);
-Vue.component('BIconFileEarmarkTextFill', BIconFileEarmarkTextFill);
-Vue.component('BIconClipboardData', BIconClipboardData);
-Vue.component('BIconBoxArrowUpRight', BIconBoxArrowUpRight);
-Vue.component('BIconPencil', BIconPencil);
-Vue.component('BIconLockFill', BIconLockFill);
-Vue.component('BIconUnlockFill', BIconUnlockFill);
-Vue.component('BIconArrowDown', BIconArrowDown);
-Vue.component('BIconBookmarkStarFill', BIconBookmarkStarFill);
-Vue.component('BIconTrash', BIconTrash);
-Vue.component('BIconKeyFill', BIconKeyFill);
-Vue.component('BIconPersonPlusFill', BIconPersonPlusFill);
+Vue.use(TablePlugin);

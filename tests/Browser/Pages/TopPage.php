c4dd4aad367d3db49521b1103e0ba9b559da1146
128na <simutrans.128na@gmail.com>
2022-09-19 21:39:23 +0900

    タイトル指定

--- tests/Browser/Pages/TopPage.php
+++ tests/Browser/Pages/TopPage.php
@@ -15,7 +15,10 @@ class TopPage extends Page
     public function __construct()
     {
         $user = User::factory()->create();
-        $this->article = Article::factory()->publish()->addonPost()->create(['user_id' => $user->id]);
+        $this->article = Article::factory()->publish()->addonPost()->create([
+            'title' => 'dummy title',
+            'user_id' => $user->id,
+        ]);
         $this->category = Category::where('type', 'pak')->where('slug', '128')->first();
         $this->article->categories()->save($this->category);
     }
@@ -38,7 +41,7 @@ class TopPage extends Page
     public function assert(Browser $browser)
     {
         $browser
-            ->waitForText($this->article->title)
+            ->waitForText($this->article->title, 30)
             ->assertSee($this->article->title)
             ->assertSee('の新着アドオン')
             ->assertSee(__("category.{$this->category->type}.{$this->category->slug}"))

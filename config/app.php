3c94aedac53ecedb6ff3245ccb35e987f77ba06c
128na <simutrans.128na@gmail.com>
2022-09-20 17:00:07 +0900

    fix provider

--- config/app.php
+++ config/app.php
@@ -160,7 +160,7 @@ return [
         Illuminate\Cache\CacheServiceProvider::class,
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
         Illuminate\Cookie\CookieServiceProvider::class,
-        // Illuminate\Database\DatabaseServiceProvider::class,
+        Illuminate\Database\DatabaseServiceProvider::class,
         Illuminate\Encryption\EncryptionServiceProvider::class,
         Illuminate\Filesystem\FilesystemServiceProvider::class,
         Illuminate\Foundation\Providers\FoundationServiceProvider::class,

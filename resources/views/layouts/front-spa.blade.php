9eb86eb8aab5bd186c91b16afd396d32981a51c4
128na <simutrans.128na@gmail.com>
2022-09-22 22:36:18 +0900

    og:imageを公式準拠に

--- resources/views/layouts/front-spa.blade.php
+++ resources/views/layouts/front-spa.blade.php
@@ -12,15 +12,15 @@
 
     <meta name="description" content="{{ $meta['description'] ?? config('app.meta-description') }}">
 
+    <meta name="twitter:card" content="{{ $meta['card_type'] ?? 'summary_large_image' }}">
+    <meta name="twitter:site" content="{{ '@' . config('app.twitter') }}">
+    <meta name="twitter:creator" content="{{ '@' . config('app.creator') }}">
+    <meta property="og:image" content="{{ $meta['image'] ?? asset(config('app.meta-image')) }}">
     <meta property="og:type" content="website">
     <meta property="og:title" content="{{ $meta['title'] ?? config('app.name') }}">
     <meta property="og:description" content="{{ $meta['description'] ?? config('app.meta-description') }}">
     <meta property="og:url" content="{{ $meta['canonical'] ?? url()->current() }}">
 
-    <meta name="twitter:card" content="{{ $meta['card_type'] ?? 'summary_large_image' }}">
-    <meta name="twitter:site" content="{{ '@' . config('app.twitter') }}">
-    <meta name="twitter:creator" content="{{ '@' . config('app.creator') }}">
-    <meta name="twitter:image" content="{{ $meta['image'] ?? asset(config('app.meta-image')) }}">
 
     {{-- 本番環境以外はnoindex nofollow --}}
     @unless(\App::environment('production'))

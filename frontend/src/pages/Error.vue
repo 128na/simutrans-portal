fb54b99a906b3053745b4993a2a1b5eff43782a0
128na <simutrans.128na@gmail.com>
2022-10-09 01:27:11 +0900

    ハンドリング調整

--- frontend/src/pages/Error.vue
+++ frontend/src/pages/Error.vue
@@ -1,8 +1,9 @@
 <template>
-  <q-img src="/images/404.png" fit="cover" class="fullscreen">
+  <q-img src="/images/404.png" fit="cover" class="fullscreen" position="0 0"
+    alt="ページが表示できない悲しみを再開発で線路が寸断され、大量発生した「ルート無し」のスクリーンショットで示しています">
     <div class="fullscreen text-center q-pa-md flex flex-center">
       <div>
-        <div style="font-size: 30vh">
+        <div style="font-size: 10rem">
           {{status}}
         </div>
 
@@ -21,15 +22,22 @@ import { defineComponent } from 'vue';
 import { useRoute } from 'vue-router';
 
 const getMessage = (status) => {
+  if (Number.isNaN(status)) {
+    return 'にゃーん🐈';
+  }
   switch (status) {
-    case '401':
+    case 401:
+      return 'ログインが必要です。';
+    case 403:
       return '権限が有りません。';
-    case '404':
+    case 404:
       return 'ページが見つかりませんでした。';
-    case '419':
-      return 'エラーが発生しました。';
-    case '429':
-      return 'アクセス頻度が高すぎます。';
+    case 418:
+      return '( ´･ω･)⊃旦';
+    case 419:
+      return 'ページの更新期限が切れました';
+    case 429:
+      return 'アクセス頻度が高すぎます。ゆっくりしていってね！！！';
     default:
       return 'エラーが発生しました。';
   }
@@ -39,7 +47,7 @@ export default defineComponent({
   name: 'Error',
   setup() {
     const route = useRoute();
-    const status = String(route.params.status || 404);
+    const status = Number.parseInt(route.params.status || 404, 10);
     const message = getMessage(status);
 
     return {

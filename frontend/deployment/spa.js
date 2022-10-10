f9e3071e32f7f5a9fd9b95c0a8c30c8f15fa23c0
128na <simutrans.128na@gmail.com>
2022-10-11 00:50:02 +0900

    sep

--- frontend/deployment/spa.js
+++ frontend/deployment/spa.js
@@ -1,16 +1,18 @@
 /* eslint-disable no-console */
 const path = require('path');
+
+const s = path.sep;
 const fs = require('fs-extra');
 const read = require('fs-readdir-recursive');
 
 {
   console.info('copy file to laravel public dir.');
   const targets = [
-    { src: path.join(__dirname, '..\\dist\\spa\\css'), dest: path.join(__dirname, '..\\..\\public\\css') },
-    { src: path.join(__dirname, '..\\dist\\spa\\js'), dest: path.join(__dirname, '..\\..\\public\\js') },
-    { src: path.join(__dirname, '..\\dist\\spa\\fonts'), dest: path.join(__dirname, '..\\..\\public\\fonts') },
-    { src: path.join(__dirname, '..\\dist\\spa\\icons'), dest: path.join(__dirname, '..\\..\\public\\icons') },
-    { src: path.join(__dirname, '..\\dist\\spa\\images'), dest: path.join(__dirname, '..\\..\\public\\images') },
+    { src: path.join(__dirname, `..${s}dist${s}spa${s}css`), dest: path.join(__dirname, `..${s}..${s}public${s}css`) },
+    { src: path.join(__dirname, `..${s}dist${s}spa${s}js`), dest: path.join(__dirname, `..${s}..${s}public${s}js`) },
+    { src: path.join(__dirname, `..${s}dist${s}spa${s}fonts`), dest: path.join(__dirname, `..${s}..${s}public${s}fonts`) },
+    { src: path.join(__dirname, `..${s}dist${s}spa${s}icons`), dest: path.join(__dirname, `..${s}..${s}public${s}icons`) },
+    { src: path.join(__dirname, `..${s}dist${s}spa${s}images`), dest: path.join(__dirname, `..${s}..${s}public${s}images`) },
   ];
   targets.forEach((target) => {
     fs.emptyDirSync(target.dest);
@@ -21,12 +23,12 @@ const read = require('fs-readdir-recursive');
 {
   console.info('mix-manifest.json generating.');
   const targets = [
-    { key: 'js\\vendor.', org: '/js/vendor.js' },
-    { key: 'js\\app.', org: '/js/app.js' },
-    { key: 'css\\app.', org: '/css/app.css' },
-    { key: 'css\\vendor.', org: '/css/vendor.css' },
+    { key: `js${s}vendor.`, org: '/js/vendor.js' },
+    { key: `js${s}app.`, org: '/js/app.js' },
+    { key: `css${s}app.`, org: '/css/app.css' },
+    { key: `css${s}vendor.`, org: '/css/vendor.css' },
   ];
-  const files = read(path.join(__dirname, '..\\..\\public'));
+  const files = read(path.join(__dirname, `..${s}..${s}public`));
   const manifest = {};
   targets.forEach((t) => {
     const item = files.find((f) => f.startsWith(t.key));
@@ -37,7 +39,7 @@ const read = require('fs-readdir-recursive');
   });
   console.log({ manifest });
   fs.writeFileSync(
-    path.join(__dirname, '..\\..\\public\\mix-manifest.json'),
+    path.join(__dirname, `..${s}..${s}public${s}mix-manifest.json`),
     JSON.stringify(manifest),
   );
 }

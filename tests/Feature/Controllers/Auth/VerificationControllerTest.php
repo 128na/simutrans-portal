5716e010b4373fdd77545f59ae19981d79acfca2
128na <simutrans.128na@gmail.com>
2022-10-15 01:04:32 +0900

    fix test

--- tests/Feature/Controllers/Auth/VerificationControllerTest.php
+++ tests/Feature/Controllers/Auth/VerificationControllerTest.php
@@ -23,14 +23,20 @@ class VerificationControllerTest extends TestCase
         $res = $this->postJson(route('api.v2.verification.resend'));
         $res->assertStatus(200);
 
+        /** @var string|null */
         $url = null;
         Notification::assertSentTo($this->user, VerifyEmail::class, function ($notification, $channels) use (&$url) {
             $url = $notification->getVerificationUrl($this->user);
 
             return true;
         });
+        $this->assertNotNull($url);
         $res = $this->get($url);
-        $res->assertRedirect(route('mypage.index'));
+        $res->assertOk();
+        $apiUrl = str_replace('mypage/verify', 'api/v2/email/verify', $url);
+
+        $res = $this->getJson($apiUrl);
+        $res->assertStatus(200);
 
         $res = $this->postJson(route('api.v2.articles.store'));
         $res->assertStatus(422);

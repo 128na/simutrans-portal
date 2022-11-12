b7bc662baffa76e9a9ea167aca020695124f93d9
128na <simutrans.128na@gmail.com>
2022-11-13 02:47:06 +0900

    コード整理

--- frontend/deployment/sourcemap.js
+++ frontend/deployment/sourcemap.js
@@ -1,24 +1,27 @@
+/* eslint-disable no-console */
 require('dotenv').config();
 const { publishSourcemap } = require('@newrelic/publish-sourcemap');
 const path = require('path');
 const read = require('fs-readdir-recursive');
 
-const s = path.sep;
+const uploadSourceMaps = (files) => {
+  for (let index = 0; index < files.length; index += 2) {
+    const file = files[index];
+    const source = files[index + 1];
+    const data = {
+      sourcemapUrl: `${process.env.BACKEND_URL}/js/${source}`,
+      javascriptUrl: `${process.env.BACKEND_URL}/js/${file}`,
+      applicationId: 601355519,
+      apiKey: process.env.NR_USER_KEY,
+      releaseName: process.env.APP_VERSION,
+      releaseId: process.env.APP_VERSION,
+      repoUrl: 'https://github.com/128na/simutrans-portal',
+    };
+    publishSourcemap(data, (err) => { console.log(err || 'Source map upload done'); });
+  }
+};
 
+const s = path.sep;
 const jsDir = path.join(__dirname, `..${s}..${s}public`, 'js');
 const files = read(jsDir);
-
-for (let index = 0; index < files.length; index += 2) {
-  const file = files[index];
-  const source = files[index + 1];
-  const data = {
-    sourcemapUrl: `${process.env.BACKEND_URL}/js/${source}`,
-    javascriptUrl: `${process.env.BACKEND_URL}/js/${file}`,
-    applicationId: 601355519,
-    apiKey: process.env.NR_APIKEY,
-    releaseName: process.env.APP_VERSION,
-    releaseId: process.env.APP_VERSION,
-    repoUrl: 'https://github.com/128na/simutrans-portal',
-  };
-  publishSourcemap(data, (err) => { console.log(err || 'Source map upload done'); });
-}
+uploadSourceMaps(files);

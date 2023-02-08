ac0793b30b746cc00c7156a9d5bc705a5eacd98a
128na <simutrans.128na@gmail.com>
2023-02-08 21:33:52 +0900

    fix test

--- tests/Feature/Controllers/Api/Front/DiscordControllerTest.php
+++ tests/Feature/Controllers/Api/Front/DiscordControllerTest.php
@@ -22,7 +22,7 @@ class DiscordControllerTest extends TestCase
             $m->shouldReceive('create')->once()->andReturn('dummy');
         });
 
-        $url = route('discord-invite');
+        $url = '/invite-simutrans-interact-meeting';
         $res = $this->postJson($url);
         $res
             ->assertOk()
@@ -37,7 +37,7 @@ class DiscordControllerTest extends TestCase
         $this->mock(InviteService::class, function (MockInterface $m) {
             $m->shouldReceive('create')->once()->andThrow(new Error('dummy'));
         });
-        $url = route('discord-invite');
+        $url = '/invite-simutrans-interact-meeting';
         $res = $this->postJson($url);
         $res
             ->assertStatus(400)
@@ -52,7 +52,7 @@ class DiscordControllerTest extends TestCase
         $this->mock(InviteService::class, function (MockInterface $m) {
             $m->shouldReceive('create')->never();
         });
-        $url = route('discord-invite');
+        $url = '/invite-simutrans-interact-meeting';
         $res = $this->postJson($url);
         $res
             ->assertStatus(400)

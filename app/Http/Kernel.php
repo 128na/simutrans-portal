259facd273fc1ad9ae03e91507569d13adedc5cb
128na <simutrans.128na@gmail.com>
2023-02-09 22:01:40 +0900

    stan

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends HttpKernel
      *
      * These middleware are run during every request to your application.
      *
-     * @var array<class-string>
+     * @var array<int, string>
      */
     protected $middleware = [
         \App\Http\Middleware\RedirectUnlessSSL::class,
@@ -27,7 +27,7 @@ class Kernel extends HttpKernel
     /**
      * The application's route middleware groups.
      *
-     * @var array<array<string>>
+     * @var array<string, array<int, string>>
      */
     protected $middlewareGroups = [
         'web' => [
@@ -53,7 +53,7 @@ class Kernel extends HttpKernel
      *
      * These middleware may be assigned to groups or used individually.
      *
-     * @var array<class-string>
+     * @var array<string, string>
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -13,7 +13,7 @@ class EventServiceProvider extends ServiceProvider
     /**
      * The event listener mappings for the application.
      *
-     * @var array<class-string, array<class-string>>
+     * @var array<string, array<int, string>>
      */
     protected $listen = [
         Registered::class => [

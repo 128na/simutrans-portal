ba9832b8d902818f50cdc631ec91de7b906cdf7e
128na <simutrans.128na@gmail.com>
2023-03-11 22:54:37 +0900

    なぜかCIだけ失敗する

--- tests/Browser/ConversionTest.php
+++ tests/Browser/ConversionTest.php
@@ -38,9 +38,9 @@ class ConversionTest extends DuskTestCase
     public function test()
     {
         $this->browse(function (Browser $browser) {
-            $dayly = now()->format('Ymd');
-            $monthly = now()->format('Ym');
-            $yearly = now()->format('Y');
+            $dayly = now('UTC')->format('Ymd');
+            $monthly = now('UTC')->format('Ym');
+            $yearly = now('UTC')->format('Y');
             $total = 'total';
 
             $this->assertDatabaseMissing('view_counts', ['article_id' => $this->article1->id, 'type' => '1', 'period' => $dayly]);
@@ -53,11 +53,8 @@ class ConversionTest extends DuskTestCase
             $this->assertDatabaseMissing('conversion_counts', ['article_id' => $this->article1->id, 'type' => '4', 'period' => $total]);
 
             $browser
-                ->visit('/')
+                ->visit("/articles/{$this->article1->slug}")
                 ->waitForText($this->article1->title)
-                ->clickLink($this->article1->title)
-                ->waitForText('掲載先URL')
-                ->assertPathIs("/articles/{$this->article1->slug}")
                 ->click('@conversion-link');
             sleep(1);
             $this->assertDatabaseHas('view_counts', ['article_id' => $this->article1->id, 'type' => '1', 'period' => $dayly, 'count' => 1]);
@@ -79,11 +76,8 @@ class ConversionTest extends DuskTestCase
             $this->assertDatabaseMissing('conversion_counts', ['article_id' => $this->article2->id, 'type' => '4', 'period' => $total]);
 
             $browser
-                ->visit('/')
+                ->visit("/articles/{$this->article2->slug}")
                 ->waitForText($this->article2->title)
-                ->clickLink($this->article2->title)
-                ->waitForText('ダウンロード')
-                ->assertPathIs("/articles/{$this->article2->slug}")
                 ->click('@conversion-download');
             sleep(1);
             $this->assertDatabaseHas('view_counts', ['article_id' => $this->article2->id, 'type' => '1', 'period' => $dayly, 'count' => 1]);

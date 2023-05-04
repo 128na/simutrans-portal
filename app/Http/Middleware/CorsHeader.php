c56ae0dc02a0c4bec7f0d25be5fda213bbfdddd5
128na <simutrans.128na@gmail.com>
2023-05-04 18:17:47 +0900

    クロスドメイン準備

--- app/Http/Middleware/CorsHeader.php
+++ app/Http/Middleware/CorsHeader.php
@@ -15,8 +15,19 @@ class CorsHeader
     public function handle(Request $request, Closure $next)
     {
         return $next($request)
-            ->header('Access-Control-Allow-Origin', config('app.url'))
+            ->header('Access-Control-Allow-Origin', $this->getAllowOrigin($request))
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', '*');
+    }
+
+    private function getAllowOrigin(Request $request): string
+    {
+        $ref = $request->server('HTTP_REFERER');
+        // for debug
+        if (str_starts_with($ref, 'https://pwa-dev.128-bit.net')) {
+            return 'https://pwa-dev.128-bit.net';
+        }
+
+        return config('app.url');
     }
 }

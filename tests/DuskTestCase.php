8fcecfde8ebbc4a364c711bb57b82dcc71ed085e
128na <simutrans.128na@gmail.com>
2024-01-14 22:04:58 +0900

    p

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -35,7 +35,7 @@ abstract class DuskTestCase extends BaseTestCase
      */
     public static function prepare()
     {
-        if (!static::runningInSail()) {
+        if (! static::runningInSail()) {
             static::startChromeDriver();
         }
     }

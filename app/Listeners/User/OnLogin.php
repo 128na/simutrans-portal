ef860c5445aabd4b7376013a09d8a31243170f2f
128na <simutrans.128na@gmail.com>
2024-02-23 22:36:38 +0900

    rememberのセッション復元はログイン判定しない

--- app/Listeners/User/OnLogin.php
+++ app/Listeners/User/OnLogin.php
@@ -18,10 +18,21 @@ class OnLogin extends BaseListener
 
     public function handle(Login $login): void
     {
-        if ($login->user instanceof User) {
+        if ($login->user instanceof User && $this->isNewLogin()) {
             $this->logger->channel('audit')->info('ログイン', $this->getUserInfo($login->user));
 
             $login->user->notify(new SendLoggedInEmail($login->user->loginHistories()->create()));
         }
+    }
+
+    private function isNewLogin(): bool
+    {
+        $traces = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS, 20);
+
+        return match (true) {
+            in_array('AuthenticatedSessionController', $traces, true) => true,
+            in_array('TwoFactorAuthenticatedSessionController', $traces, true) => true,
+            default => false,
+        };
     }
 }

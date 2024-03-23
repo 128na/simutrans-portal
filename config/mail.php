004555a8ab65caeab3b6e564d8f46c58cd1f891c
128na <simutrans.128na@gmail.com>
2024-03-23 16:22:17 +0900

    env整理

--- config/mail.php
+++ config/mail.php
@@ -71,8 +71,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@simutrans-portal.128-bit.net'),
+        'name' => env('MAIL_FROM_NAME', 'Simutrans Addon Portal'),
     ],
 
     /*

6bef459e86cf51bcdbdc2d101a610c57e48aef97
128na <simutrans.128na@gmail.com>
2024-03-24 13:32:09 +0900

    スクショ並び替えをドラッグに変更

--- frontend/src/components/Mypage/Screenshot/FormAttachments.vue
+++ frontend/src/components/Mypage/Screenshot/FormAttachments.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="row q-gutter-sm">
-    <template v-for="(attachment, index) in  editor.screenshot.attachments " :key="attachment.id">
-      <div>
-        <a :href="mypage.findAttachmentById(attachment.id)?.url" target="_blank" rel="noreferrer noopener">
-          <q-img :src="mypage.findAttachmentById(attachment.id)?.url" width="256px" />
+  <draggable v-model="editor.screenshot.attachments" item-key="id" class="row q-gutter-sm">
+    <template #item="{ element, index }">
+      <div class="">
+        <a :href="mypage.findAttachmentById(element.id)?.url" target="_blank" rel="noreferrer noopener">
+          <q-img :src="mypage.findAttachmentById(element.id)?.url" width="256px" />
         </a>
         <q-input v-model="editor.screenshot.attachments[index].caption" label-slot bottom-slots
           :error-message="editor.vali(`screenshot.attachments.${index}.caption`)"
@@ -13,11 +13,9 @@
           </template>
         </q-input>
       </div>
-      <div class="column" v-if="index !== editor.screenshot.attachments.length - 1">
-        <q-btn icon="swap_horiz" flat color="secondary" class="col" @click="swap(index, index + 1)" />
-      </div>
     </template>
-  </div>
+  </draggable>
+
   <div class="q-my-md">
     <FileManager v-model="attachmentIds" :onlyImage="true" attachmentableType="Screenshot"
       :attachmentableId="editor.screenshot.id" />
@@ -29,12 +27,14 @@ import { useScreenshotEditStore } from 'src/store/screenshotEdit';
 import { useMypageStore } from 'src/store/mypage';
 import LabelOptional from 'src/components/Common/LabelOptional.vue';
 import FileManager from 'src/components/Mypage/FileManager.vue';
+import draggable from 'vuedraggable';
 
 export default defineComponent({
   name: 'FormAttachments',
   components: {
     FileManager,
     LabelOptional,
+    draggable,
   },
   props: {
   },
@@ -50,22 +50,17 @@ export default defineComponent({
       },
     });
     watchEffect(() => {
-      // 現在のオーダー順にソート
-      editor.screenshot.attachments.sort((a, b) => a.order - b.order);
       // 表示順をオーダーに適用
-      editor.screenshot.attachments.map((a, index) => ({ ...a, order: index + 1 }));
+      editor.screenshot.attachments.map((a, index) => {
+        a.order = index + 1;
+        return a;
+      });
     });
-
-    const swap = (a, b) => {
-      editor.screenshot.attachments[a].order = b;
-      editor.screenshot.attachments[b].order = a;
-    };
 
     return {
       mypage,
       editor,
       attachmentIds,
-      swap,
     };
   },
 });

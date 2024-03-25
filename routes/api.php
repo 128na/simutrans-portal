3d0af43fe84ca1add609c8c45df4fadd808f65f4
GitHub Action <actions@github.com>
2024-03-25 14:33:21 +0000

    [rector] Rector fixes

--- routes/api.php
+++ routes/api.php
@@ -5,5 +5,5 @@ declare(strict_types=1);
 use App\Http\Controllers\Api\Front\ConversionController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('conversion/{article}', [ConversionController::class, 'conversion']);
-Route::post('shown/{article}', [ConversionController::class, 'shown']);
+Route::post('conversion/{article}', (new ConversionController())->conversion(...));
+Route::post('shown/{article}', (new ConversionController())->shown(...));

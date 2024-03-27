449149c7b98d0f68834e5a03022b362d2c9f3b04
128na <simutrans.128na@gmail.com>
2024-03-28 00:43:14 +0900

    ScreenshotController

---
+++ tests/Feature/Controllers/Web/ScreenshotControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature\Controllers\Web;
+
+use App\Models\Screenshot;
+use Tests\Feature\TestCase;
+
+class ScreenshotControllerTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function testIndex(): void
+    {
+        $testResponse = $this->get(route('screenshots.index'));
+
+        $testResponse->assertOk();
+    }
+
+    public function testShow(): void
+    {
+        $screenshot = Screenshot::factory()->create();
+        $testResponse = $this->get(route('screenshots.show', $screenshot));
+
+        $testResponse->assertOk();
+    }
+}

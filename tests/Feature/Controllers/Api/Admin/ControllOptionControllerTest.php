5a159de6e7d10e6d971b8d7cfc5a987acdcb16ac
128na <simutrans.128na@gmail.com>
2024-03-28 23:44:14 +0900

    factoryのないクラスはトレイと削除する

--- tests/Feature/Controllers/Api/Admin/ControllOptionControllerTest.php
+++ tests/Feature/Controllers/Api/Admin/ControllOptionControllerTest.php
@@ -2,20 +2,20 @@
 
 declare(strict_types=1);
 
-namespace Tests\OldFeature\Controllers\Api\Admin;
+namespace Tests\Feature\Controllers\Api\Admin;
 
 use App\Enums\UserRole;
-use App\Models\ControllOption;
-use Tests\TestCase;
+use App\Models\User;
+use Tests\Feature\TestCase;
 
 class ControllOptionControllerTest extends TestCase
 {
-    private ControllOption $controllOption;
+    private User $user;
 
     protected function setUp(): void
     {
         parent::setUp();
-        $this->controllOption = ControllOption::create(['key' => 'dummy', 'value' => true]);
+        $this->user = User::factory()->admin()->create();
     }
 
     public function testIndex(): void

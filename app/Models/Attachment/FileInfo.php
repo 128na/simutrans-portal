abca8e7576f6cc07330ee6e5bc6ed9066735018f
128na <simutrans.128na@gmail.com>
2024-03-28 23:47:17 +0900

    factoryトレイと削除

--- app/Models/Attachment/FileInfo.php
+++ app/Models/Attachment/FileInfo.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace App\Models\Attachment;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class FileInfo extends Model
 {
-    use HasFactory;
-
     protected $fillable = [
         'attachment_id',
         'data',

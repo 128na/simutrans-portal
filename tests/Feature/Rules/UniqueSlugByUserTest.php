14eef80780e19024a4f201bb471198944fb9951b
128na <simutrans.128na@gmail.com>
2024-03-31 00:50:43 +0900

    add

--- tests/Feature/Rules/UniqueSlugByUserTest.php
+++ tests/Feature/Rules/UniqueSlugByUserTest.php
@@ -2,13 +2,13 @@
 
 namespace Tests\Feature\Rules;
 
+use App\Enums\UserRole;
+use App\Models\Article;
 use App\Models\User;
 use App\Rules\UniqueSlugByUser;
 use Closure;
 use Illuminate\Translation\PotentiallyTranslatedString;
 use Mockery\MockInterface;
-use PHPUnit\Framework\Attributes\DataProvider;
-use PHPUnit\Framework\Attributes\Test;
 use Tests\Feature\TestCase;
 
 class UniqueSlugByUserTest extends TestCase
@@ -38,20 +38,81 @@ class UniqueSlugByUserTest extends TestCase
         return new UniqueSlugByUser();
     }
 
-    #[Test]
-    #[DataProvider('data')]
-    public function test(Closure $setup, bool $expected): void
+    public function test_一般投稿、編集中記事と重複_OK(): void
     {
-        $value = $setup($this);
+        $article = Article::factory()->create(['user_id' => $this->user->id]);
         $this->actingAs($this->user);
         $this->getSUT()
-            ->validate('dummy', $value, $this->failClosure);
-        $this->assertEquals($expected, $this->failCalled);
+            ->setData(['article' => ['id' => $article->id]])
+            ->validate('dummy', $article->slug, $this->failClosure);
+        $this->assertFalse($this->failCalled);
     }
 
-    public static function data(): \Generator
+    public function test_一般投稿、自身の既存記事と重複_NG(): void
     {
-        yield 'ok' => [fn (self $self) => $self->createImageAttachment(User::factory()->create())->id, false];
-        yield 'ng' => [fn (self $self) => $self->createAttachment(User::factory()->create())->id, true];
+        $article = Article::factory()->create(['user_id' => $this->user->id]);
+        $this->actingAs($this->user);
+        $this->getSUT()
+            ->validate('dummy', $article->slug, $this->failClosure);
+        $this->assertTrue($this->failCalled);
+    }
+
+    public function test_一般投稿、他者の記事と重複_OK(): void
+    {
+        $article = Article::factory()->create(['user_id' => User::factory()->create()->id]);
+        $this->actingAs($this->user);
+        $this->getSUT()
+            ->validate('dummy', $article->slug, $this->failClosure);
+        $this->assertFalse($this->failCalled);
+    }
+
+    public function test_一般投稿、管理者の記事と重複_NG(): void
+    {
+        $article = Article::factory()->create(['user_id' => User::factory()->admin()->create()->id]);
+        $this->actingAs($this->user);
+        $this->getSUT()
+            ->validate('dummy', $article->slug, $this->failClosure);
+        $this->assertTrue($this->failCalled);
+    }
+
+    public function test_管理者投稿、編集中の記事と重複_OK(): void
+    {
+        $this->user->update(['role' => UserRole::Admin]);
+        $article = Article::factory()->create(['user_id' => $this->user->id]);
+        $this->actingAs($this->user);
+        $this->getSUT()
+            ->setData(['article' => ['id' => $article->id]])
+            ->validate('dummy', $article->slug, $this->failClosure);
+        $this->assertFalse($this->failCalled);
+    }
+
+    public function test_管理者投稿、自身の他記事と重複_NG(): void
+    {
+        $this->user->update(['role' => UserRole::Admin]);
+        $article = Article::factory()->create(['user_id' => $this->user->id]);
+        $this->actingAs($this->user);
+        $this->getSUT()
+            ->validate('dummy', $article->slug, $this->failClosure);
+        $this->assertTrue($this->failCalled);
+    }
+
+    public function test_管理者投稿、他者の記事と重複_NG(): void
+    {
+        $this->user->update(['role' => UserRole::Admin]);
+        $article = Article::factory()->create();
+        $this->actingAs($this->user);
+        $this->getSUT()
+            ->validate('dummy', $article->slug, $this->failClosure);
+        $this->assertTrue($this->failCalled);
+    }
+
+    public function test_管理者投稿、管理者の記事と重複_NG(): void
+    {
+        $this->user->update(['role' => UserRole::Admin]);
+        $article = Article::factory()->create(['user_id' => User::factory()->admin()->create()->id]);
+        $this->actingAs($this->user);
+        $this->getSUT()
+            ->validate('dummy', $article->slug, $this->failClosure);
+        $this->assertTrue($this->failCalled);
     }
 }

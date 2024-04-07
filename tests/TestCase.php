48d1b4be4c7049b5e10ba32eed20cc856243a42e
128na <simutrans.128na@gmail.com>
2024-04-08 00:09:14 +0900

    bypass readonly (PHPUnit11不可)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,4 +8,8 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
 }

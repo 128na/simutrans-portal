f7de07fbbd4cad1e3b29ced0a3adca2410b8b689
128na <simutrans.128na@gmail.com>
2024-04-08 01:00:02 +0900

    sleep:fakeは全テスト適用

--- tests/Feature/Commands/CommandCheckDeadLinkTest.php
+++ tests/Feature/Commands/CommandCheckDeadLinkTest.php
@@ -4,14 +4,12 @@ declare(strict_types=1);
 
 namespace Tests\Feature\Commands;
 
-use Illuminate\Support\Sleep;
 use Tests\Feature\TestCase;
 
 final class CommandCheckDeadLinkTest extends TestCase
 {
     public function test(): void
     {
-        Sleep::fake();
         $res = $this->artisan('check:deadlink');
         $res->assertExitCode(0);
     }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,11 +5,14 @@ declare(strict_types=1);
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Sleep;
 
 abstract class TestCase extends BaseTestCase
 {
     protected function setUp(): void
     {
         parent::setUp();
+
+        Sleep::fake();
     }
 }

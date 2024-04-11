f59d8ee6da7612967c69d3ac633dd0a6c8da17f2
128na <simutrans.128na@gmail.com>
2024-04-12 00:16:57 +0900

    self

--- app/Models/User.php
+++ app/Models/User.php
@@ -129,7 +129,7 @@ final class User extends Authenticatable implements BulkZippableInterface, MustV
      */
     public function invited(): BelongsTo
     {
-        return $this->belongsTo(User::class, 'invited_by');
+        return $this->belongsTo(self::class, 'invited_by');
     }
 
     /**
@@ -137,7 +137,7 @@ final class User extends Authenticatable implements BulkZippableInterface, MustV
      */
     public function invites(): HasMany
     {
-        return $this->hasMany(User::class, 'invited_by');
+        return $this->hasMany(self::class, 'invited_by');
     }
 
     /**
@@ -145,7 +145,7 @@ final class User extends Authenticatable implements BulkZippableInterface, MustV
      */
     public function invitesReclusive(): HasMany
     {
-        return $this->hasMany(User::class, 'invited_by')->with(['invites']);
+        return $this->hasMany(self::class, 'invited_by')->with(['invites']);
     }
 
     /**

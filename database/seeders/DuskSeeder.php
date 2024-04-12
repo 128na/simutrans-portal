e1a82f453807d06ffa722e85c25b93a314175eb1
128na <simutrans.128na@gmail.com>
2024-04-12 23:24:44 +0900

    del

--- database/seeders/DuskSeeder.php
+++ database/seeders/DuskSeeder.php
@@ -4,21 +4,19 @@ declare(strict_types=1);
 
 namespace Database\Seeders;
 
+use App\Models\Article;
+use App\Models\User;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
 
 /**
  * Duskテスト用シーダー
  */
 final class DuskSeeder extends Seeder
 {
-    /**
-     * 管理者とカテゴリを追加する.
-     */
     public function run(): void
     {
-        DB::statement('delete from articles');
-        DB::statement('delete from users');
+        Article::truncate();
+        User::truncate();
         $this->call(CategorySeeder::class);
         $this->call(ControllOptionsSeeder::class);
     }

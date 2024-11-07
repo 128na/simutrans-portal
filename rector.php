1bd22fc124eaa65984308f484312e0adb56da428
128na <simutrans.128na@gmail.com>
2024-11-07 23:57:15 +0900

    設定確定

--- rector.php
+++ rector.php
@@ -35,15 +35,14 @@ return RectorConfig::configure()
     ])
     ->withSets([
         LaravelLevelSetList::UP_TO_LARAVEL_110,
-
         LaravelSetList::LARAVEL_ARRAYACCESS_TO_METHOD_CALL,
         LaravelSetList::LARAVEL_ARRAY_STR_FUNCTION_TO_STATIC_CALL,
         LaravelSetList::LARAVEL_CODE_QUALITY,
         LaravelSetList::LARAVEL_COLLECTION,
         LaravelSetList::LARAVEL_CONTAINER_STRING_TO_FULLY_QUALIFIED_NAME,
-        // LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER,
-        // LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
-        // LaravelSetList::LARAVEL_IF_HELPERS,
-        // LaravelSetList::LARAVEL_LEGACY_FACTORIES_TO_CLASSES,
-        // LaravelSetList::LARAVEL_STATIC_TO_INJECTION,
+        // LaravelSetList::LARAVEL_ELOQUENT_MAGIC_METHOD_TO_QUERY_BUILDER, 完全クラス名が冗長なのでなし
+        LaravelSetList::LARAVEL_FACADE_ALIASES_TO_FULL_NAMES,
+        // LaravelSetList::LARAVEL_IF_HELPERS, phpstanがちょくちょくエラーになるので
+        LaravelSetList::LARAVEL_LEGACY_FACTORIES_TO_CLASSES,
+        // LaravelSetList::LARAVEL_STATIC_TO_INJECTION, ぶっ壊れる
     ]);

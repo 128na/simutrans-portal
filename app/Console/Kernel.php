49af719a701c11bef8c73d6050c4fee90920f9e4
128na <simutrans.128na@gmail.com>
2024-12-05 18:34:01 +0900

    スケジュールロックするのでonOneServerを使用しない

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 final class Kernel extends ConsoleKernel
 {
+    private const EXPIRES_AT = 10;
     /**
      * Define the application's command schedule.
      */
@@ -18,39 +19,31 @@ final class Kernel extends ConsoleKernel
         // 毎分 サーバー都合でcron設定としては5分周期
         $schedule->command('queue:cron')->everyMinute()
             ->runInBackground()
-            ->withoutOverlapping()
-            ->onOneServer();
+            ->withoutOverlapping(self::EXPIRES_AT);
 
         $schedule->command('article:publish-reservation')->everyMinute()
             ->runInBackground()
-            ->withoutOverlapping()
-            ->onOneServer();
+            ->withoutOverlapping(self::EXPIRES_AT);
 
         // 毎時
         $schedule->command('article:ranking')->hourly()
-            ->withoutOverlapping()
-            ->onOneServer();
+            ->withoutOverlapping(self::EXPIRES_AT);
         $schedule->command('article:json')->hourly()
-            ->withoutOverlapping()
-            ->onOneServer();
+            ->withoutOverlapping(self::EXPIRES_AT);
 
         // 毎日
         $schedule->command('check:deadlink')->dailyAt('10:00')
             ->runInBackground()
-            ->withoutOverlapping()
-            ->onOneServer();
+            ->withoutOverlapping(self::EXPIRES_AT);
         $schedule->command('backup:clean')->dailyAt('2:00')
             ->runInBackground()
-            ->withoutOverlapping()
-            ->onOneServer();
+            ->withoutOverlapping(self::EXPIRES_AT);
         $schedule->command('backup:run')->dailyAt('3:00')
             ->runInBackground()
-            ->withoutOverlapping()
-            ->onOneServer();
+            ->withoutOverlapping(self::EXPIRES_AT);
         $schedule->command('sitemap:generate')->dailyAt('5:00')
             ->runInBackground()
-            ->withoutOverlapping()
-            ->onOneServer();
+            ->withoutOverlapping(self::EXPIRES_AT);
     }
 
     /**

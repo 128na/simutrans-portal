e4d179a9b9b7bdb0762a7c24f3b53434f5f97f3d
128na <simutrans.128na@gmail.com>
2024-12-07 13:25:33 +0900

    cronうまく動かないので調整

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,7 +9,19 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 final class Kernel extends ConsoleKernel
 {
-    private const int EXPIRES_AT = 10;
+    /**
+     * 設置サーバーのcron実行間隔。レンサバだと毎分に設定できないぞい
+     */
+    private const int CRON_INTERVAL = 2;
+
+    private function getLogPath(): string
+    {
+        return storage_path(sprintf(
+            'logs%sschedule-%s.log',
+            DIRECTORY_SEPARATOR,
+            now()->toDateString()
+        ));
+    }
 
     /**
      * Define the application's command schedule.
@@ -17,34 +29,36 @@ final class Kernel extends ConsoleKernel
     #[\Override]
     protected function schedule(Schedule $schedule): void
     {
-        // 毎分 サーバー都合でcron設定としては5分周期
-        $schedule->command('queue:cron')->everyMinute()
-            ->runInBackground()
-            ->withoutOverlapping(self::EXPIRES_AT);
-
-        $schedule->command('article:publish-reservation')->everyMinute()
-            ->runInBackground()
-            ->withoutOverlapping(self::EXPIRES_AT);
-
-        // 毎時
-        $schedule->command('article:ranking')->hourly()
-            ->withoutOverlapping(self::EXPIRES_AT);
-        $schedule->command('article:json')->hourly()
-            ->withoutOverlapping(self::EXPIRES_AT);
+        $startAt = now()->toDateTimeString();
+        logger()->channel('worker')->info('Kernel::schedule start', ['startAt' => $startAt]);
+        $output = $this->getLogPath();
 
         // 毎日
         $schedule->command('check:deadlink')->dailyAt('10:00')
-            ->runInBackground()
-            ->withoutOverlapping(self::EXPIRES_AT);
+            ->appendOutputTo($output);
         $schedule->command('backup:clean')->dailyAt('2:00')
-            ->runInBackground()
-            ->withoutOverlapping(self::EXPIRES_AT);
+            ->appendOutputTo($output);
         $schedule->command('backup:run')->dailyAt('3:00')
-            ->runInBackground()
-            ->withoutOverlapping(self::EXPIRES_AT);
+            ->appendOutputTo($output);
         $schedule->command('sitemap:generate')->dailyAt('5:00')
-            ->runInBackground()
-            ->withoutOverlapping(self::EXPIRES_AT);
+            ->appendOutputTo($output);
+
+        // 毎時
+        $schedule->command('article:ranking')->hourly()
+            ->appendOutputTo($output);
+        $schedule->command('article:json')->hourly()
+            ->appendOutputTo($output);
+
+        // 毎分 サーバー都合でcron設定としては2分周期
+        $schedule->command('article:publish-reservation')->everyMinute()
+            ->appendOutputTo($output);
+        $schedule->command('queue:work', [
+            '--stop-when-empty',
+            '--max-time' => (int)(self::CRON_INTERVAL / 2),
+        ])->everyMinute()
+            ->appendOutputTo($output);
+
+        logger()->channel('worker')->info('Kernel::schedule end', ['startAt' => $startAt]);
     }
 
     /**
@@ -53,7 +67,7 @@ final class Kernel extends ConsoleKernel
     #[\Override]
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }

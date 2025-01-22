e41383114aac45030c8c1eaedffb34c03346fab3
128na <simutrans.128na@gmail.com>
2025-01-22 22:11:35 +0900

    ATTR_PERSISTENTを削除

--- config/database.php
+++ config/database.php
@@ -52,7 +52,6 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                PDO::ATTR_PERSISTENT => env('DB_PERSISTENT', true),
             ]) : [],
 
             /*
@@ -112,7 +111,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'predis'),
-            'prefix' => Str::slug(env('APP_NAME', 'laravel'), '_').'_database_',
+            'prefix' => Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_',
         ],
 
         'default' => [

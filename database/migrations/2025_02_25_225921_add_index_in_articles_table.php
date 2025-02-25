dae127a034d7b9e3ebc761cce0c62f788cd1f850
128na <simutrans.128na@gmail.com>
2025-02-25 23:10:03 +0900

    add index

---
+++ database/migrations/2025_02_25_225921_add_index_in_articles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->unique(['user_id', 'slug']);
+            $table->index(['deleted_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('articles', function (Blueprint $table) {
+            $table->dropUnique(['user_id', 'slug']);
+            $table->dropIndex(['deleted_at']);
+        });
+    }
+};

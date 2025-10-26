9d64b08b6f1bedc828d7f0a47e91e6ecd507679d
128na <simutrans.128na@gmail.com>
2025-10-26 09:19:19 +0900

    fix meta

--- app/Http/Controllers/v2/UserController.php
+++ app/Http/Controllers/v2/UserController.php
@@ -8,14 +8,22 @@ use App\Actions\User\Registration;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\User\StoreRequest;
 use App\Models\User;
+use App\Services\Front\MetaOgpService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Auth;
 
 final class UserController extends Controller
 {
+    public function __construct(
+        private readonly MetaOgpService $metaOgpService,
+    ) {}
+
     public function showInvite(User $user): \Illuminate\Contracts\View\View
     {
-        return view('v2.user.invite', ['invitee' => $user]);
+        return view('v2.user.invite', [
+            'invitee' => $user,
+            'meta' => $this->metaOgpService->registration(),
+        ]);
     }
 
     public function registration(User $user, StoreRequest $storeRequest, Registration $registration): \Illuminate\Contracts\View\View
@@ -23,7 +31,10 @@ final class UserController extends Controller
         $data = $storeRequest->validated();
         $inviter = $registration($data, $user);
 
-        return view('v2.user.welcome', ['inviter' => $inviter]);
+        return view('v2.user.welcome', [
+            'inviter' => $inviter,
+            'meta' => $this->metaOgpService->login(),
+        ]);
     }
 
     public function showLogin(): \Illuminate\Contracts\View\View|RedirectResponse
@@ -32,21 +43,30 @@ final class UserController extends Controller
             return to_route('mypage.index');
         }
 
-        return view('v2.user.login');
+        return view('v2.user.login', [
+            'meta' => $this->metaOgpService->login(),
+        ]);
     }
 
     public function showTwoFactor(): \Illuminate\Contracts\View\View|RedirectResponse
     {
-        return view('v2.user.two-factor');
+        return view('v2.user.two-factor', [
+            'meta' => $this->metaOgpService->login(),
+        ]);
     }
 
     public function showForgotPassword(): \Illuminate\Contracts\View\View|RedirectResponse
     {
-        return view('v2.user.forget-password');
+        return view('v2.user.forget-password', [
+            'meta' => $this->metaOgpService->login(),
+        ]);
     }
 
     public function showResetPassword(string $token): \Illuminate\Contracts\View\View|RedirectResponse
     {
-        return view('v2.user.reset-password', ['token' => $token]);
+        return view('v2.user.reset-password', [
+            'token' => $token,
+            'meta' => $this->metaOgpService->resetPassword(),
+        ]);
     }
 }
--- app/Services/Front/MetaOgpService.php
+++ app/Services/Front/MetaOgpService.php
@@ -17,7 +17,7 @@ final class MetaOgpService
     public function show(User $user, Article $article): array
     {
         return [
-            'title' => $article->title.' - '.Config::string('app.name'),
+            'title' => $article->title . ' - ' . Config::string('app.name'),
             'description' => $this->trimDescription($article->contents->getDescription()),
             'image' => $article->has_thumbnail ? $article->thumbnail_url : null,
             'canonical' => route('articles.show', ['userIdOrNickname' => $user->nickname ?? $user->id, 'articleSlug' => $article->slug]),
@@ -31,8 +31,8 @@ final class MetaOgpService
     public function pak(string $name): array
     {
         return [
-            'title' => Lang::get('category.pak.'.$name).' - '.Config::string('app.name'),
-            'description' => Lang::get('category.description.pak.'.$name),
+            'title' => Lang::get('category.pak.' . $name) . ' - ' . Config::string('app.name'),
+            'description' => Lang::get('category.description.pak.' . $name),
         ];
     }
 
@@ -42,7 +42,7 @@ final class MetaOgpService
     public function announces(): array
     {
         return [
-            'title' => 'お知らせ'.' - '.Config::string('app.name'),
+            'title' => 'お知らせ' . ' - ' . Config::string('app.name'),
             'description' => '運営からのお知らせです。',
         ];
     }
@@ -53,7 +53,7 @@ final class MetaOgpService
     public function pages(): array
     {
         return [
-            'title' => '記事'.' - '.Config::string('app.name'),
+            'title' => '記事' . ' - ' . Config::string('app.name'),
             'description' => 'アドオン以外の記事です。',
         ];
     }
@@ -64,7 +64,7 @@ final class MetaOgpService
     public function search(): array
     {
         return [
-            'title' => '検索'.' - '.Config::string('app.name'),
+            'title' => '検索' . ' - ' . Config::string('app.name'),
             'description' => '記事の検索結果です。',
         ];
     }
@@ -75,8 +75,59 @@ final class MetaOgpService
     public function social(): array
     {
         return [
-            'title' => 'SNS・通知ツール'.' - '.Config::string('app.name'),
+            'title' => 'SNS・通知ツール' . ' - ' . Config::string('app.name'),
             'description' => '記事投稿や更新通知を受け取ることができるSNSアカウントやツールです。',
+        ];
+    }
+
+    /**
+     * @return array{title:string}
+     */
+    public function discord(): array
+    {
+        return [
+            'title' => 'Discord招待リンクの発行' . ' - ' . Config::string('app.name'),
+        ];
+    }
+
+    /**
+     * @return array{title:string}
+     */
+    public function login(): array
+    {
+        return [
+            'title' => 'ログイン' . ' - ' . Config::string('app.name'),
+        ];
+    }
+
+    /**
+     * @return array{title:string}
+     */
+    public function registration(): array
+    {
+        return [
+            'title' => 'ユーザー登録' . ' - ' . Config::string('app.name'),
+        ];
+    }
+
+    /**
+     * @return array{title:string}
+     */
+    public function resetPassword(): array
+    {
+        return [
+            'title' => 'パスワードリセット' . ' - ' . Config::string('app.name'),
+        ];
+    }
+
+
+    /**
+     * @return array{title:string}
+     */
+    public function mypage(): array
+    {
+        return [
+            'title' => 'マイページ' . ' - ' . Config::string('app.name'),
         ];
     }
 

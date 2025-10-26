db5a095aaa0e4658e56d94e9bd50ac572c9ab3a0
128na <simutrans.128na@gmail.com>
2025-10-27 00:55:41 +0900

    wip

--- resources/views/v2/mypage/tags.blade.php
+++ resources/views/v2/mypage/tags.blade.php
@@ -6,7 +6,7 @@
         <h2 class="text-3xl font-semibold text-pretty text-gray-900 sm:text-3xl">タグの編集</h2>
         <p class="mt-2 text-gray-600">
             作成済みタグの説明文を編集できます。<br />
-            タグの削除はできません。紐づく記事が１つもなくなると自動的に削除されます。
+            タグの削除はできません。紐づく記事が１つもなくなると数日後に自動削除されます。
         </p>
     </div>
     <div class="mt-10 flex flex-col gap-y-12 border-t border-gray-200 pt-10 sm:mt-8 sm:pt-8 lg:mx-0">

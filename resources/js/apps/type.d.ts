133417beb9b7fd5434e7f2b0458ad313500c89ae
128na <simutrans.128na@gmail.com>
2025-10-31 01:35:44 +0900

    記事一覧

--- resources/js/apps/type.d.ts
+++ resources/js/apps/type.d.ts
@@ -20,3 +20,107 @@ type SimpleUser = {
   id: number;
   name: string;
 };
+
+type Count = {
+  id: number;
+  article_id: number;
+  user_id: number;
+  type: 1 | 2 | 3 | 4;
+  period: string;
+  count: number;
+};
+
+type ListingArticle = {
+  id: number;
+  user_id: number;
+  title: string;
+  slug: string;
+  post_type: "addon-post" | "addon-introduction" | "page" | "markdown";
+  status: "publish" | "reservation" | "draft" | "trash" | "private";
+  attachments: Attachment[];
+  total_conversion_count: Count | null;
+  total_view_count: Count | null;
+  published_at: string | null;
+  modified_at: string;
+};
+
+type Attachment = {
+  id: number;
+  user_id: number;
+  attachmentable_id: number;
+  attachmentable_type: "App\\Models\\Article" | "App\\Models\\Profile";
+  caption: string | null;
+  original_name: string;
+  order: number;
+  created_at: string;
+  updated_at: string;
+};
+
+type User = SimpleUser & {
+  nickname: string | null;
+};
+
+type EditArticle = {
+  id: number;
+  user_id: number;
+  title: string;
+  slug: string;
+  post_type: "addon-post" | "addon-introduction" | "page" | "markdown";
+  status: "publish" | "reservation" | "draft" | "trash" | "private";
+  contents:
+    | ContentAddonPost
+    | ContentAddonIntroduction
+    | ContentPage
+    | ContentMarkdown;
+  published_at: string | null;
+  modified_at: string;
+  created_at: string;
+  updated_at: string;
+};
+
+type Content = {
+  thumbnail: number;
+};
+type ContentAddonPost = Content & {
+  description: string;
+  file: number;
+  author: string | null;
+  license: string | null;
+  thanks: string | null;
+};
+type ContentAddonIntroduction = Content & {
+  description: string;
+  link: string;
+  author: string;
+  license: string | null;
+  thanks: string | null;
+  agreement: boolean;
+  exclude_link_check: boolean;
+};
+type ContentMarkdown = Content & {
+  markdown: string;
+};
+type ContentPage = Content & {
+  sections: (SectionText | SectionImage | SectionCaption | SectionUrl)[];
+};
+
+type Section = {
+  type: string;
+};
+type SectionText = Section & {
+  type: "text";
+  text: string;
+};
+type SectionImage = Section & {
+  type: "image";
+  image: number;
+};
+type SectionUrl = Section & {
+  type: "url";
+  url: string;
+};
+
+type SectionCaption = Section & {
+  type: "caption";
+  caption: string;
+};
---
+++ resources/views/v2/mypage/articles.blade.php
@@ -0,0 +1,11 @@
+@extends('v2.mypage.layout')
+@section('max-w', 'max-w-7xl')
+@section('content')
+<div class="mx-auto max-w-7xl p-6 lg:px-8">
+    <div>
+        <h2 class="text-3xl font-semibold text-pretty text-gray-900 sm:text-3xl">記事一覧</h2>
+    </div>
+    <div class="mt-10 flex flex-col gap-y-12 border-t border-gray-200 pt-10 sm:mt-8 sm:pt-8 lg:mx-0">
+        <div id="app-article-list" data-user='@json($user)' data-articles='@json($articles)'></div>
+    </div>
+    @endsection

1e50e29223a2eb6eb6722ddca2d34b5816a25427
128na <simutrans.128na@gmail.com>
2025-11-03 10:52:47 +0900

    カテゴリの表示条件

--- resources/js/apps/features/articles/SelectCategories.tsx
+++ resources/js/apps/features/articles/SelectCategories.tsx
@@ -22,26 +22,48 @@ export const SelectCategories = ({
   className,
   onSelect,
 }: Props) => {
+  const pak128Id = categories["pak"].find((c) => c.slug === "128")?.id;
+  const toolIds = categories["addon"]
+    .filter((c) => c.slug.includes("-tools"))
+    .map((c) => c.id);
+  const categoryGroupFilter = (type: CategoryType): boolean => {
+    // pak.128 選択時のみ pak128_position を表示
+    if (type === "pak128_position") {
+      return pak128Id && selected.includes(pak128Id) ? true : false;
+    }
+    // addon.*-tools 選択時のみ double_slope を表示
+    if (type === "double_slope") {
+      return selected.some((id) => toolIds.includes(id));
+    }
+    return only?.includes(type) ?? true;
+  };
+  const categoryFilter = (c: Category.Search): boolean => {
+    // 管理者権限なら全部
+    if (showAdmin) {
+      return true;
+    }
+    // 一般ユーザーは通常カテゴリのみ
+    return !c.need_admin;
+  };
+
   return typedKeys(categories)
-    .filter((type) => only?.includes(type))
+    .filter(categoryGroupFilter)
     .map((type) => (
       <div key={type} className="mb-6">
         <div className={twMerge("text-sm text-gray-900", typeClassName)}>
           {t(`category.type.${type}`)}
         </div>
-        {categories[type]
-          .filter((c) => showAdmin || !c.need_admin)
-          .map((category) => (
-            <Checkbox
-              className={className}
-              key={category.id}
-              value={category.id}
-              checked={selected.includes(category.id)}
-              onChange={() => onSelect(category)}
-            >
-              {t(`category.${category.type}.${category.slug}`)}
-            </Checkbox>
-          ))}
+        {categories[type].filter(categoryFilter).map((category) => (
+          <Checkbox
+            className={className}
+            key={category.id}
+            value={category.id}
+            checked={selected.includes(category.id)}
+            onChange={() => onSelect(category)}
+          >
+            {t(`category.${category.type}.${category.slug}`)}
+          </Checkbox>
+        ))}
       </div>
     ));
 };

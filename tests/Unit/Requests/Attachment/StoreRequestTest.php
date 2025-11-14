45560e74441ce57ea712271f239d291ddb6b4259
128na <simutrans.128na@gmail.com>
2025-11-15 02:01:43 +0900

    unit

--- tests/Unit/Requests/Attachment/StoreRequestTest.php
+++ tests/Unit/Requests/Attachment/StoreRequestTest.php
@@ -27,47 +27,20 @@ final class StoreRequestTest extends TestCase
 
     public static function dataFail(): \Generator
     {
-        yield 'filesがnull' => [
-            ['files' => null],
-            'files',
+        yield 'fileが空' => [
+            ['file' => null],
+            'file',
         ];
-        yield 'filesが空' => [
-            ['files' => []],
-            'files',
-        ];
-        yield 'files.*がファイル以外' => [
-            ['files' => ['test.zip']],
-            'files.0',
-        ];
-        yield '画像のみで画像以外' => [
-            ['only_image' => 1, 'files' => [UploadedFile::fake()->create('test.zip', 1, 'application/zip')]],
-            'files.0',
-        ];
-        yield 'crop.topが129以上' => [
-            ['only_image' => 1, 'crop' => ['top' => 129]],
-            'crop.top',
-        ];
-        yield 'crop.bottomが129以上' => [
-            ['only_image' => 1, 'crop' => ['bottom' => 129]],
-            'crop.bottom',
-        ];
-        yield 'crop.leftが129以上' => [
-            ['only_image' => 1, 'crop' => ['left' => 129]],
-            'crop.left',
-        ];
-        yield 'crop.rightが129以上' => [
-            ['only_image' => 1, 'crop' => ['right' => 129]],
-            'crop.right',
+        yield 'file.*がファイル以外' => [
+            ['file' => 'test.zip'],
+            'file',
         ];
     }
 
     public static function dataPass(): \Generator
     {
         yield '指定なしで通常ファイル' => [
-            ['files' => [UploadedFile::fake()->create('test.zip', 1, 'application/zip')]],
-        ];
-        yield '画像のみで画像' => [
-            ['only_image' => 1, 'files' => [UploadedFile::fake()->image('test.png', 1)]],
+            ['file' => UploadedFile::fake()->create('test.zip', 1, 'application/zip')],
         ];
     }
 }

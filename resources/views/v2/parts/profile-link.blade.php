7c9963bb4d6cfcfa2a31d798fecf5cc96ff28b36
128na <simutrans.128na@gmail.com>
2025-11-16 18:37:28 +0900

    add github

--- resources/views/v2/parts/profile-link.blade.php
+++ resources/views/v2/parts/profile-link.blade.php
@@ -19,6 +19,10 @@ break;
 case str_starts_with($website, 'https://www.nicovideo.jp/'):
 $title = 'Niconico';
 break;
+// その他有名サイト
+case str_starts_with($website, 'https://github.com/'):
+$title = 'GitHub';
+break;
 }
 @endphp
 @include('v2.parts.link-external', ['url' => $website, 'title' => $title])

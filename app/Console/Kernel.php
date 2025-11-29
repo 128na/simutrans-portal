dc9ca524ae35bd3c1b602cc1eeb719474e0aa102
128na <simutrans.128na@gmail.com>
2025-11-29 14:49:59 +0900

    format

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -53,7 +53,7 @@ final class Kernel extends ConsoleKernel
     #[\Override]
     protected function commands(): void
     {
-        $this->load(__DIR__ . '/Commands');
+        $this->load(__DIR__.'/Commands');
 
         require base_path('routes/console.php');
     }

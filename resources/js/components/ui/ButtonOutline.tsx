f5ceafa5f4c0416ca1740b7e060d7725f7baadb8
128na <simutrans.128na@gmail.com>
2025-12-07 02:10:07 +0900

    fix button

--- resources/js/components/ui/ButtonOutline.tsx
+++ resources/js/components/ui/ButtonOutline.tsx
@@ -15,7 +15,7 @@ export default function ButtonOutline({
       type="button"
       className={twMerge(
         className,
-        "px-3 py-1.5 text-sm border border-g2 text-g4 bg-g2/50 hover:bg-g4 hover:text-white"
+        "px-3 py-1.5 text-sm border border-g2 text-g4 bg-g2/50 hover:bg-g4 hover:text-white disabled:bg-g1"
       )}
       {...props}
     >

4af3e03e44c689537fc189ab7cfe969811fb8bd4
128na <simutrans.128na@gmail.com>
2025-12-06 13:33:21 +0900

    レイアウト調整

--- resources/js/features/articles/components/ArticleList.tsx
+++ resources/js/features/articles/components/ArticleList.tsx
@@ -30,7 +30,9 @@ export const ArticleList = ({ articles }: Props) => {
                 {article.modified_at} ({article.published_at} 投稿)
               </div>
               <h3 className="title-md">
-                <a href={article.url}>{article.title}</a>
+                <a href={article.url} className="hover:text-gray-700">
+                  {article.title}
+                </a>
               </h3>
               <div className="text-sm text-gray-600 line-clamp-3 break-all">
                 {article.description}

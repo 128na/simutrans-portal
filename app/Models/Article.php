2d54c3674347361b65605e64082e561af8bd9f1d
GitHub Action <actions@github.com>
2025-12-12 15:40:06 +0000

    [rector] Rector fixes

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -39,8 +39,10 @@ use Spatie\Feed\FeedItem;
  * @property-read bool $hasFile
  * @property-read Collection<int, Category> $categoryPaks
  * @property-read bool $hasThumbnail
+ *
  * @method static Builder<Article> page()
  * @method static Builder<Article> pak(string $slug)
+ *
  * @mixin IdeHelperArticle
  */
 final class Article extends Model implements Feedable
